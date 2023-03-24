c07a4b993a7969feeb6b07d147f27583f33f9988
1lyasm <il.mystafa+1@gmail.com>
2023-03-24 16:19:22 +0400

    modify test_dsa.cpp

--- tests/test_dsa.cpp
+++ tests/test_dsa.cpp
@@ -6,5 +6,5 @@ TEST(ReverseInteger, reverse) {
   EXPECT_EQ(ReverseInteger::reverse(123), 321);
   EXPECT_EQ(ReverseInteger::reverse(120), 21);
   EXPECT_EQ(ReverseInteger::reverse(-123), -321);
-  EXPECT_EQ(ReverseInteger::reverse(1534236469), 1);
+  EXPECT_EQ(ReverseInteger::reverse(1534236469), 0);
 }
