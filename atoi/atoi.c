bc78ee284ae7405d6ad85108588e3525550fbe26
1lyasm <il.mystafa+1@gmail.com>
2023-04-10 18:18:37 +0400

    add atoi

---
+++ atoi/atoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void ignore_ws(char *s, int *pos) {
+    while (s[*pos] == ' ') ++*pos;
+}
+
+int myAtoi(char * s){
+    int pos = 0;
+    ignore_ws(s, &pos);
+    handle_sign();
+    printf("pos: %d", pos);
+    return -1;
+}
+
+int main() {
+    myAtoi(" oy");
+    return 0;
+}
