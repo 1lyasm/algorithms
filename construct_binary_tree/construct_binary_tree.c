1b72361a9f283712011fc1606fa2ee6f9517cf99
1lyasm <il.mystafa+1@gmail.com>
2023-04-12 14:22:09 +0400

    modify construct_binary_tree

--- construct_binary_tree/construct_binary_tree.c
+++ construct_binary_tree/construct_binary_tree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "construct_binary_tree.h"
 
 static int find(int *arr, int len, int target) {
@@ -12,12 +13,18 @@ static struct TreeNode *buildSubtree(int *pre, int preLen, int *in,
     int inLen, int preInd) {
     struct TreeNode *root;
     int inRootPos = -1;
-    if (preInd >= preLen || inLen < 1) {
+    if (inLen < 1) {
         return 0;
     }
-    for (; preInd < preLen &&
-        (inRootPos = find(in, inLen, pre[preInd])) == inLen; ++preInd);
-    if (preInd >= preLen) return 0;
+    for (; ; ++preInd) {
+        if (preInd >= preLen) {
+            return 0;
+        }
+        inRootPos = find(in, inLen, pre[preInd]);
+        if (inRootPos != inLen) {
+            break;
+        }
+    }
     root = calloc(1, sizeof(struct TreeNode));
     root->val = pre[preInd];
     root->left = buildSubtree(pre, preLen, in, inRootPos, preInd + 1);
@@ -28,8 +35,6 @@ static struct TreeNode *buildSubtree(int *pre, int preLen, int *in,
 
 struct TreeNode* buildTree(int* preorder, int preorderSize, int* inorder,
     int inorderSize) {
-    int preInd = 0;
-    struct TreeNode *root = buildSubtree(preorder, preorderSize,
-        inorder, inorderSize, preInd);
-    return root;
+    return buildSubtree(preorder, preorderSize,
+        inorder, inorderSize, 0);
 }
