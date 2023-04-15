02e5a223b31a020077d26c6a788ed1af7369a0ef
1lyasm <il.mystafa@gmail.com>
2023-04-15 00:36:14 +0000

    format all

--- bst_insert/bst_insert.h
+++ bst_insert/bst_insert.h
@@ -7,7 +7,8 @@ struct TreeNode {
   TreeNode *right;
   TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 #endif
