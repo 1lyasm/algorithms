629dd54f63841f4675a7ebe32153a97e6f5198af
1lyasm <il.mystafa+1@gmail.com>
2023-04-15 13:11:48 +0400

    simplify binary_tree_inorder

--- binary_tree_inorder/binary_tree_inorder_iter.cpp
+++ binary_tree_inorder/binary_tree_inorder_iter.cpp
@@ -6,31 +6,19 @@
 
 class Solution {
 public:
-  struct StackItem {
-    TreeNode *node;
-    bool was_visited;
-    StackItem(TreeNode *node) : node(node), was_visited(false) {}
-  };
-
   std::vector<int> inorderTraversal(TreeNode *root) {
     std::vector<int> traversal;
-    if (root == nullptr) {
-      return traversal;
-    }
-    std::vector<StackItem> stack;
-    stack.push_back(StackItem(root));
-    while (!stack.empty()) {
-      if (!stack.back().was_visited && stack.back().node->left != nullptr) {
-        stack.back().was_visited = true;
-        stack.push_back(StackItem(stack.back().node->left));
-      } else {
-        TreeNode *leftMost = stack.back().node;
-        traversal.push_back(leftMost->val);
-        stack.pop_back();
-        if (leftMost->right != nullptr) {
-          stack.push_back(StackItem(leftMost->right));
-        }
+    std::vector<TreeNode *> stack;
+    TreeNode *cur = root;
+    while (cur != nullptr || !stack.empty()) {
+      while (cur != nullptr) {
+        stack.push_back(cur);
+        cur = cur->left;
       }
+      traversal.push_back(stack.back()->val);
+      cur = stack.back();
+      stack.pop_back();
+      cur = cur->right;
     }
     return traversal;
   }
