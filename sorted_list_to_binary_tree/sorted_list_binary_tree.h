d1be07b9af205f496947390ef0197ab758a9e1ae
1lyasm <il.mystafa+1@gmail.com>
2023-04-17 18:45:44 +0400

    complete sorted_list_to_binary_tree

---
+++ sorted_list_to_binary_tree/sorted_list_binary_tree.h
@@ -0,0 +1,22 @@
+#ifndef SORTED_LIST_TO_BINARY_TREE_H
+#define SORTED_LIST_TO_BINARY_TREE_H
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+#endif
---
+++ sorted_list_to_binary_tree/sorted_list_to_binary_tree.cpp
@@ -0,0 +1,57 @@
+#include "sorted_list_binary_tree.h"
+
+#include <deque>
+#include <vector>
+
+class Solution {
+public:
+  int list_size(ListNode *head) {
+    ListNode *it = head;
+    int count = 0;
+    while (it) {
+      it = it->next;
+      ++count;
+    }
+    return count;
+  }
+
+  TreeNode *sortedListToBST(ListNode *head) {
+    if (head == nullptr) {
+      return nullptr;
+    }
+    TreeNode *root = new TreeNode(0);
+    int counter = 1;
+    std::deque<TreeNode *> parents = {root};
+    int nodeCount = list_size(head);
+    while (counter < nodeCount) {
+      parents.front()->left = new TreeNode(counter);
+      parents.push_back(parents.front()->left);
+      ++counter;
+      if (counter >= nodeCount) {
+        break;
+      }
+      parents.front()->right = new TreeNode(counter);
+      parents.push_back(parents.front()->right);
+      ++counter;
+      if (counter >= nodeCount) {
+        break;
+      }
+      parents.pop_front();
+    }
+    std::vector<TreeNode *> stack;
+    ListNode *list_it = head;
+    TreeNode *tree_it = root;
+    while (tree_it != nullptr || !stack.empty()) {
+      while (tree_it != nullptr) {
+        stack.push_back(tree_it);
+        tree_it = tree_it->left;
+      }
+      stack.back()->val = list_it->val;
+      list_it = list_it->next;
+      tree_it = stack.back();
+      stack.pop_back();
+      tree_it = tree_it->right;
+    }
+    return root;
+  }
+};
