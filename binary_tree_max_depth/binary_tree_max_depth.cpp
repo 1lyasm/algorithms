315cad2e8c6774c929c2772a786442394dd70942
1lyasm <il.mystafa+1@gmail.com>
2023-04-18 09:43:12 +0400

    complete binary_tree_max_depth

---
+++ binary_tree_max_depth/binary_tree_max_depth.cpp
@@ -0,0 +1,12 @@
+#include "binary_tree_max_depth.h"
+#include <algorithm>
+
+class Solution {
+public:
+  int maxDepth(TreeNode *root) {
+    if (root == nullptr) {
+      return 0;
+    }
+    return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+  }
+};
