80c4d02419edd6759cadf49c8baa44aca99032aa
1lyasm <il.mystafa+1@gmail.com>
2023-04-24 19:49:54 +0400

    [validate_bst] change solution

--- validate_bst/Solution.java
+++ validate_bst/Solution.java
@@ -26,22 +26,19 @@ class TreeNode {
 }
 
 class Solution {
-  public boolean isValidBST(TreeNode root) {
-    var stack = new Stack<TreeNode>();
-    var cur = root;
-    TreeNode prev = null;
-    while (cur != null || !stack.isEmpty()) {
-      while (cur != null) {
-        stack.push(cur);
-        cur = cur.left;
-      }
-      cur = stack.pop();
-      if (prev != null && cur.val <= prev.val) {
-        return false;
-      }
-      prev = cur;
-      cur = cur.right;
+  public boolean isValidSubBST(TreeNode root, long min, long max) {
+    if (root == null) {
+      return true;
     }
-    return true;
+    if (root.val <= min || root.val >= max) {
+      return false;
+    }
+    return isValidSubBST(root.left, min, root.val) &&
+        isValidSubBST(root.right, root.val, max);
+  }
+
+  public boolean isValidBST(TreeNode root) {
+    return isValidSubBST(root, (long) Integer.MIN_VALUE - 1L,
+        (long) Integer.MAX_VALUE + 1L);
   }
 }
