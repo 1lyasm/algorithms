9923848e104928b9c2785bf44da805a31c651a35
Ilyas Mustafazade <84722851+1lyasm@users.noreply.github.com>
2023-05-13 00:34:12 +0400

    Update format_all.py

--- format_all.py
+++ format_all.py
@@ -7,4 +7,4 @@ for subdir, dirs, _ in os.walk("."):
         for file_ in files:
           if os.path.splitext(file_)[1] in [".cpp", ".c", ".h", ".java", ".cppm"]:
             print(f"formatting {file_}")
-            os.system(f"clang-format -i {dir}/{file_}")
+            os.system(f"clang-format -i -style=Google {dir}/{file_}")
