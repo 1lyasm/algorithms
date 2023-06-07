07f12aa716771e9f9c312da8d6036a72e438f183
1lyasm <il.mystafa+1@gmail.com>
2023-06-07 22:06:18 +0400

    make faster

--- string_combination/main.c
+++ string_combination/main.c
@@ -3,39 +3,49 @@
 #include <string.h>
 #include <time.h>
 
-void genCombinations(char *s, int n, int k, int *indices, int *combinationCount) {
-	int i;
-	int incrementedIndex = k - 1;
-	++*combinationCount;
-	for (i = 0; i < k; ++i) {
-		printf("%c", s[indices[i]]);
-	}
-	printf("\n");
-	while (++indices[incrementedIndex] == n && incrementedIndex != 0) {
-		indices[incrementedIndex--] = 0;
-	}
-	if (indices[0] == n) {
-		return;
-	}
-	genCombinations(s, n, k, indices, combinationCount);
-}
-
-int main() {
-	char s[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'}; 
-	int n = sizeof(s);
-	int k = 4;
-	int *indices = malloc(k * sizeof(int));
+void genCombinations(char *s, int n, int k) {
 	int combinationCount = 0;
-	clock_t start;
-	memset(indices, 0, k * sizeof(int));
+	int *indices = malloc(k * sizeof(int));
 	if (indices == 0) {
 		printf("\nmalloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	start = clock();
-	genCombinations(s, n, k, indices, &combinationCount);
-	printf("\n\ngeneration duration: %ld\n", clock() - start);
-	printf("\n\ncombination count: %d\n", combinationCount);
+	memset(indices, 0, k * sizeof(int));
+	while (indices[0] != n) {
+		int i;
+		int incrementedIndex;
+		for (i = 0; i < k; ++i)
+			putchar(s[indices[i]]);
+		putchar('\n');
+		for (incrementedIndex = k - 1; ++indices[incrementedIndex] == n && 
+				incrementedIndex != 0;) {
+			indices[incrementedIndex--] = 0;
+		}
+		++combinationCount;
+	}
 	free(indices);
+	printf("\n\ncombination count: %d\n", combinationCount);
+}
+
+int main() {
+	char s[] = "abcdefg"; 
+	int n = 7;
+	int k = 4;
+	clock_t start;
+	int bufferSize = 16384;
+	char *buffer = malloc(bufferSize * sizeof(char));
+	if (buffer == 0) {
+		fputs("\nmalloc failed\n", stdout);
+		exit(EXIT_FAILURE);
+	}
+	if (setvbuf(stdout, buffer, _IOFBF, bufferSize) != 0) {
+		fputs("\nsetvbuf failed\n", stdout);
+		exit(EXIT_FAILURE);
+	}
+	start = clock();
+	genCombinations(s, n, k);
+	printf("\n\ngeneration duration: %ld\n", clock() - start);
+	fclose(stdout);
+	free(buffer);
 	return 0;
 }
