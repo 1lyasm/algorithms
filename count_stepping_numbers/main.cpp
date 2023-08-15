b002958d58df62ae8eaa901db0cdcb24fac805d0
1lyasm <il.mystafa@gmail.com>
2023-08-15 10:58:33 +0400

    Reorder functions, add testSeekNonStepping

--- count_stepping_numbers/main.cpp
+++ count_stepping_numbers/main.cpp
@@ -3,10 +3,42 @@
 #include <string>
 
 class Solution {
-  static void leftPad(std::string& Str, unsigned long Cnt) {
-    for (unsigned long I = 0; I < Cnt; ++I) {
-      Str.insert(0, "0");
+ public:
+  static int countSteppingNumbers(std::string low, std::string high) {
+    if (low.length() < high.length()) {
+      leftPad(low, high.length() - low.length());
     }
+    int NonZeroIdx = seekNonZero(low);
+    int NonSteppingIdx = seekNonStepping(low);
+    return 0;
+  }
+
+  static void testCountSteppingNumbers() {
+    assert(countSteppingNumbers("1", "11") == 10);
+    assert(countSteppingNumbers("90", "101") == 2);
+  }
+
+  static int seekNonStepping(std::string& Str) {
+    for (int i = 0; i < Str.size() - 1; ++i) {
+      if (Str[i] - '0' != Str[i + 1] - '0' - 1 &&
+          Str[i] - '0' != Str[i + 1] - '0' + 1) {
+        std::cout << "res: " << i + 1 << "\n";
+        return i + 1;
+      }
+    }
+    std::cout << "res: " << -1 << "\n";
+    return -1;
+  }
+
+  static void testSeekNonStepping() {
+    std::string Str = std::string("102");
+    assert(seekNonStepping(Str) == 2);
+    Str = std::string("1013");
+    assert(seekNonStepping(Str) == 3);
+    Str = std::string("19");
+    assert(seekNonStepping(Str) == 1);
+    Str = std::string("9890");
+    assert(seekNonStepping(Str) == 3);
   }
 
   static int seekNonZero(const std::string& Str) {
@@ -23,18 +55,21 @@ class Solution {
     }
   }
 
- public:
-  static int countSteppingNumbers(std::string low, std::string high) {
-    if (low.length() < high.length()) {
-      leftPad(low, high.length() - low.length());
-    }
-    int NonZeroIdx = seekNonZero(low);
-    return 0;
+  static void testSeekNonZero() {
+    std::string Str = std::string("01");
+    assert(seekNonZero(Str) == 1);
+    Str = std::string("001");
+    assert(seekNonZero(Str) == 2);
+    Str = std::string("000");
+    assert(seekNonZero(Str) == -1);
+    Str = std::string("10");
+    assert(seekNonZero(Str) == 0);
   }
 
-  static void testCountSteppingNumbers() {
-    assert(countSteppingNumbers("1", "11") == 10);
-    assert(countSteppingNumbers("90", "101") == 2);
+  static void leftPad(std::string& Str, unsigned long Cnt) {
+    for (unsigned long I = 0; I < Cnt; ++I) {
+      Str.insert(0, "0");
+    }
   }
 
   static void testLeftPad() {
@@ -46,21 +81,11 @@ class Solution {
     leftPad(Str, 1);
     assert(Str == "0001");
   }
-
-  static void testSeekNonZero() {
-    std::string Str = std::string("01");
-    assert(seekNonZero(Str) == 1);
-    Str = std::string("001");
-    assert(seekNonZero(Str) == 2);
-    Str = std::string("000");
-    assert(seekNonZero(Str) == -1);
-    Str = std::string("10");
-    assert(seekNonZero(Str) == 0);
-  }
 };
 
 int main() {
   Solution::testLeftPad();
   Solution::testSeekNonZero();
-  // Solution::testCountSteppingNumbers();
+  Solution::testSeekNonStepping();
+  Solution::testCountSteppingNumbers();
 }
