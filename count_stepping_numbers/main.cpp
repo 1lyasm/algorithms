8095f198188c69bffa6fe946ddf46de8b96c2b38
1lyasm <il.mystafa@gmail.com>
2023-08-16 20:05:04 +0400

    Complete count_stepping_numbers

--- count_stepping_numbers/main.cpp
+++ count_stepping_numbers/main.cpp
@@ -1,90 +1,183 @@
 #include <cassert>
+#include <chrono>
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
+
+#define MODULO (1000000000 + 7)
 
 class Solution {
  public:
   static int countSteppingNumbers(std::string low, std::string high) {
-    long long int Cnt = 0;
-    for (int i = low[0] - '0'; i <= high[0] - '0'; ++i) {
-    //   std::string Substr = low.substr(1, low.size() - 1);
-    //   std::string Lower = std::string(Substr), Higher = std::string(Substr);
-    //   Lower[0] = Lower[0] - 1;
-    //   Higher[0] = Higher[0] + 1;
-    //   Cnt += countSubtree(Lower, high) + countSubtree(Higher, high);
-      low[0] = static_cast<char>(i + 1 + '0');
+    std::unordered_map<std::string, long long> Lookup;
+    if (low.size() == high.size()) {
+      return static_cast<int>(countSameLen(low, high, &Lookup));
     }
-    return 0;
+    long long Cnt = 0;
+    Cnt += countSameLen(low, std::string(low.size(), '9'), &Lookup);
+    for (std::string::size_type I = low.size() + 1; I < high.size(); ++I) {
+      std::string LBound = "1";
+      LBound.append(I - 1, '0');
+      Cnt += countSameLen(LBound, std::string(I, '9'), &Lookup);
+    }
+    std::string LBound = "1";
+    LBound.append(high.size() - 1, '0');
+    Cnt += countSameLen(LBound, high, &Lookup);
+    return static_cast<int>(Cnt % MODULO);
   }
 
   static void testCountSteppingNumbers() {
+    assert(countSteppingNumbers("100", "999") == 32);
     assert(countSteppingNumbers("1", "11") == 10);
     assert(countSteppingNumbers("90", "101") == 2);
+    assert(countSteppingNumbers("1", "10000000") == 881);
+    assert(countSteppingNumbers("96", "322") == 9);
+    assert(countSteppingNumbers("129", "439") == 10);
+    auto Begin = std::chrono::system_clock::now();
+    assert(countSteppingNumbers(
+               "1",
+               "999999999999999999999999999999999999999999999999999999999999999"
+               "9999999999999999999999999999999999999") == 125046265);
+    auto End = std::chrono::system_clock::now();
+    std::chrono::duration<double, std::milli> Diff = End - Begin;
+    std::cout << "Duration: " << Diff.count() << "\n";
   }
 
-  static int countSubtree(std::string Prefix, std::string &Str, std::string &High) {
-    std::string FullStr = std::string(Prefix);
-    FullStr.append(Str);
-    if ((Str.size() > 0 && (Str[0] < '0' || Str[0] > '9')) ||
-        cmp(FullStr, High) == 1) {
+  static int countSameLen(const std::string& low, const std::string& high,
+                          std::unordered_map<std::string, long long>* Lookup) {
+    long long Cnt = 0;
+    std::string Str = low;
+    for (int I = Str[0] - '0'; I <= high[0] - '0'; ++I) {
+      std::string Prefix = "";
+      Prefix = Str[0];
+      std::string Lower = Str.substr(1, Str.size() - 1),
+                  Higher = Str.substr(1, Str.size() - 1);
+      Lower[0] = Str[0] - 1;
+      Higher[0] = Str[0] + 1;
+      //   std::cout << "Prefix: " << Prefix << ", Lower: " << Lower
+      //             << ", Higher: " << Higher << ", low: " << low
+      //             << ", high: " << high << "\n";
+      if (!Lower.empty()) {
+        if (Lower[0] >= '0') {
+          Cnt += countSubtree(Prefix, Lower, low, high, Lookup);
+        }
+        if (Higher[0] <= '9') {
+          Cnt += countSubtree(Prefix, Higher, low, high, Lookup);
+        }
+      } else {
+        Cnt += 1;
+      }
+      Str[0] = static_cast<char>(I + 1 + '0');
+    }
+    // std::cout << "Cnt: " << Cnt << "\n";
+    return Cnt % MODULO;
+  }
+
+  static void testCountSameLen() {
+    std::unordered_map<std::string, long long> Lookup;
+    assert(countSameLen("33000000", "37000000", &Lookup) == 61);
+    assert(countSameLen("3000000", "7000000", &Lookup) == 234);
+    assert(countSameLen("1000000", "8999999", &Lookup) == 404);
+    assert(countSameLen("1", "9", &Lookup) == 9);
+    assert(countSameLen("100", "999", &Lookup) == 32);
+  }
+
+  static int countSubtree(std::string& Prefix, std::string& Str,
+                          const std::string& Low, const std::string& High,
+                          std::unordered_map<std::string, long long>* Lookup) {
+    // std::cout << "Prefix: " << Prefix << ", Str: " << Str << ", Low: " << Low
+    //           << ", High: " << High << "\n";
+    std::string FullStr = Prefix;
+    FullStr.push_back(Str[0]);
+    if (cmp(FullStr, High.substr(0, FullStr.size())) == 1 ||
+        cmp(FullStr, Low.substr(0, FullStr.size())) == -1) {
       return 0;
     } else if (Str.size() == 1) {
       return 1;
     }
-    std::string Substr = Str.substr(1, Str.size() - 1);
-    std::string Lower = std::string(Substr), Higher = std::string(Substr);
-    std::string NewPrefix = std::string(Prefix);
+    std::string Key = Str;
+    Key.append("?");
+    if (Str.size() == High.size()) {
+      Key.append(High.substr(High.size() - Str.size(), Str.size()));
+    } else {
+      Key.append(std::string(Str.size(), '9'));
+    }
+    auto It = Lookup->find(Key);
+    if (It != Lookup->end() &&
+        Prefix != High.substr(0, High.size() - Str.size())) {
+      //   long long Taken = (*It).second;
+      //   std::cout << "Taking " << Key << " from Lookup: " << Taken << "\n";
+      return (*It).second % MODULO;
+    }
+    std::string NewPrefix = Prefix;
     NewPrefix.push_back(Str[0]);
-    Lower[0] = Str[0] - 1;
-    Higher[0] = Str[0] + 1;
-    int Res = countSubtree(NewPrefix, Lower, High) + countSubtree(NewPrefix, Higher, High);
-    return Res;
+    long long Res = 0;
+    if (Str[0] > '0') {
+      std::string Lower = Str.substr(1, Str.size() - 1);
+      Lower[0] = Str[0] - 1;
+      Res += countSubtree(NewPrefix, Lower, Low, High, Lookup);
+    }
+    if (Str[0] < '9') {
+      std::string Higher = Str.substr(1, Str.size() - 1);
+      Higher[0] = Str[0] + 1;
+      Res += countSubtree(NewPrefix, Higher, Low, High, Lookup);
+    }
+    if (FullStr[0] != Low[0] && FullStr != Low.substr(0, FullStr.size())) {
+      (*Lookup)[Key] = Res;
+    }
+    // std::cout << "Wrote " << Key << " to Lookup: " << Res << "\n";
+    return Res % MODULO;
   }
 
   static void testCountSubtree() {
-    std::string Str = std::string("100"), High = std::string("200"),    
-        Prefix = std::string();
-    int Res = countSubtree(Prefix, Str, High);
-    std::cout << "Res: " << Res << "\n";
-    assert(Res == 3);
-    Str = std::string("2023");
-    High = std::string("2999");
-    Res = countSubtree(Prefix, Str, High);
-    std::cout << "Res: " << Res << "\n";
-    assert(Res == 7);
-    Str = std::string("100000");
-    High = std::string("199999");
-    Res = countSubtree(Prefix, Str, High);
-    std::cout << "Res: " << Res << "\n";
-    assert(Res == 20);
-    Str = std::string("100000");
-    High = std::string("120000");
-    Res = countSubtree(Prefix, Str, High);
-    std::cout << "Res: " << Res << "\n";
-    assert(Res == 6);
+    {
+      std::unordered_map<std::string, long long> Lookup;
+      std::string Prefix = "", Low = "10", Str = Low, High = "19";
+      assert(countSubtree(Prefix, Str, Low, High, &Lookup) == 2);
+    }
+    {
+      std::unordered_map<std::string, long long> Lookup;
+      std::string Prefix = "", Low = "100", Str = Low, High = "200";
+      assert(countSubtree(Prefix, Str, Low, High, &Lookup) == 3);
+    }
+    {
+      std::unordered_map<std::string, long long> Lookup;
+      std::string Prefix = "", Low = "2023", Str = Low, High = "2999";
+      assert(countSubtree(Prefix, Str, Low, High, &Lookup) == 7);
+    }
+    {
+      std::unordered_map<std::string, long long> Lookup;
+      std::string Prefix = "", Low = "100000", Str = Low, High = "199999";
+      assert(countSubtree(Prefix, Str, Low, High, &Lookup) == 20);
+    }
+    {
+      std::unordered_map<std::string, long long> Lookup;
+      std::string Prefix = "", Low = "100000", Str = Low, High = "120000";
+      assert(countSubtree(Prefix, Str, Low, High, &Lookup) == 6);
+    }
+    {
+      std::unordered_map<std::string, long long> Lookup;
+      std::string Prefix = "", Low = "120000", Str = Low, High = "199999";
+      assert(countSubtree(Prefix, Str, Low, High, &Lookup) == 14);
+    }
+    {
+      std::unordered_map<std::string, long long> Lookup;
+      std::string Prefix = "", Low = "33000000", Str = Low, High = "37000000";
+      assert(countSubtree(Prefix, Str, Low, High, &Lookup) == 61);
+    }
+    {
+      std::unordered_map<std::string, long long> Lookup;
+      std::string Prefix = "", Low = "106", Str = Low, High = "121";
+      assert(countSubtree(Prefix, Str, Low, High, &Lookup) == 1);
+    }
   }
 
-  static int seekNonStepping(std::string &Str) {
-    for (std::string::size_type i = 0; i < Str.size() - 1; ++i) {
-      if (Str[i] - '0' != Str[i + 1] - '0' - 1 &&
-          Str[i] - '0' != Str[i + 1] - '0' + 1) {
-        return static_cast<int>(i + 1);
-      }
-    }
-    return -1;
-  }
-
-  static int cmp(std::string &Str1, std::string &Str2) {
-    std::cout << "cmp called with: " << Str1 << " and " << Str2 << "\n";
-    if (Str1.size() < Str2.size()) {
+  static int cmp(const std::string& Str1, const std::string& Str2) {
+    for (std::string::size_type I = 0; I < Str1.size(); ++I) {
+      if (Str1[I] < Str2[I]) {
         return -1;
-    } else if (Str1.size() > Str2.size()) {
-        return 1;
-    }
-    for (std::string::size_type i = 0; i < Str1.size(); ++i) {
-      if (Str1[i] < Str2[i]) {
-        return -1;
-      } else if (Str1[i] > Str2[i]) {
+      } else if (Str1[I] > Str2[I]) {
         return 1;
       }
     }
@@ -92,71 +185,16 @@ class Solution {
   }
 
   static void testCmp() {
-    std::string Str1 = std::string("123"), Str2 = std::string("124");
-    assert(cmp(Str1, Str2) == -1);
-    assert(cmp(Str2, Str1) == 1);
-    Str2 = std::string("123");
-    assert(cmp(Str1, Str2) == 0);
-  }
-
-  static void testSeekNonStepping() {
-    std::string Str = std::string("102");
-    assert(seekNonStepping(Str) == 2);
-    Str = std::string("1013");
-    assert(seekNonStepping(Str) == 3);
-    Str = std::string("19");
-    assert(seekNonStepping(Str) == 1);
-    Str = std::string("9890");
-    assert(seekNonStepping(Str) == 3);
-  }
-
-  static int seekNonZero(const std::string &Str) {
-    std::string::size_type I;
-    for (I = 0; I < Str.size(); ++I) {
-      if (Str[I] != '0') {
-        break;
-      }
-    }
-    if (I != Str.size()) {
-      return static_cast<int>(I);
-    } else {
-      return -1;
-    }
-  }
-
-  static void testSeekNonZero() {
-    std::string Str = std::string("01");
-    assert(seekNonZero(Str) == 1);
-    Str = std::string("001");
-    assert(seekNonZero(Str) == 2);
-    Str = std::string("000");
-    assert(seekNonZero(Str) == -1);
-    Str = std::string("10");
-    assert(seekNonZero(Str) == 0);
-  }
-
-  static void leftPad(std::string &Str, unsigned long Cnt) {
-    for (unsigned long I = 0; I < Cnt; ++I) {
-      Str.insert(0, "0");
-    }
-  }
-
-  static void testLeftPad() {
-    std::string Str = std::string("1");
-    leftPad(Str, 2);
-    assert(Str == "001");
-    leftPad(Str, 0);
-    assert(Str == "001");
-    leftPad(Str, 1);
-    assert(Str == "0001");
+    assert(cmp("123", "124") == -1);
+    assert(cmp("124", "123") == 1);
+    assert(cmp("123", "123") == 0);
   }
 };
 
 int main() {
-  Solution::testLeftPad();
-  Solution::testSeekNonZero();
-  Solution::testSeekNonStepping();
   Solution::testCmp();
   Solution::testCountSubtree();
-  //   Solution::testCountSteppingNumbers();
+  Solution::testCountSameLen();
+  Solution::testCountSteppingNumbers();
+  return 0;
 }
