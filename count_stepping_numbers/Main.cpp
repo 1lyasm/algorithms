f4ce72bd7089e40cdf3e121cb8699bb750b8c73b
1lyasm <il.mystafa@gmail.com>
2023-08-19 13:16:11 +0400

    Add LookupFast

--- count_stepping_numbers/Main.cpp
+++ count_stepping_numbers/Main.cpp
@@ -1,8 +1,6 @@
 #include <cassert>
 #include <chrono>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
+#include <climits>
 #include <iostream>
 #include <map>
 #include <string>
@@ -14,186 +12,156 @@
 #define MODULO (1000000000 + 7)
 
 class Solution {
-  [[noreturn]] static void fail(std::string Msg) {
-    std::cout << Msg << " failed\n";
-    std::exit(EXIT_FAILURE);
-  }
-
  public:
-  static int countSteppingNumbers(std::string low, std::string high) {
-    unsigned long LowSize = low.size(), HighSize = high.size();
-    char* Low = static_cast<char*>(std::malloc((LowSize + 1) * sizeof(char)));
-    if (Low == nullptr) {
-      fail("countSteppingNumbers: std::malloc");
-    }
-    Low[LowSize] = 0;
-    std::memcpy(Low, low.c_str(), LowSize);
-    char* High = static_cast<char*>(std::malloc((HighSize + 1) * sizeof(char)));
-    if (High == nullptr) {
-      fail("countSteppingNumbers: std::malloc");
-    }
-    High[HighSize] = 0;
-    std::memcpy(High, high.c_str(), HighSize);
-    std::unordered_map<std::string, long long> Lookup;
-    int Res;
-    if (LowSize == HighSize) {
-      Res = countSameLen(Low, LowSize, High, HighSize, &Lookup) % MODULO;
-    } else {
-      unsigned long RBoundSize = HighSize - 1;
-      char* RBound =
-          static_cast<char*>(std::malloc((RBoundSize + 1) * sizeof(char)));
-      if (RBound == nullptr) {
-        fail("countSteppingNumbers: std::malloc");
-      }
-      RBound[RBoundSize] = 0;
-      std::memset(RBound, '9', RBoundSize);
-      long long Cnt = countSameLen(Low, LowSize, RBound, LowSize, &Lookup);
-      unsigned long LBoundSize = HighSize;
-      char* LBound =
-          static_cast<char*>(std::malloc((LBoundSize + 1) * sizeof(char)));
-      if (LBound == nullptr) {
-        fail("countSteppingNumbers: std::malloc");
-      }
-      LBound[LBoundSize] = 0;
-      LBound[0] = '1';
-      std::memset(LBound + 1, '0', LBoundSize - 1);
-      for (unsigned long I = LowSize + 1; I < HighSize; ++I) {
-        Cnt += countSameLen(LBound, I, RBound, I, &Lookup);
-        Cnt %= MODULO;
-      }
-      Cnt += countSameLen(LBound, LBoundSize, High, HighSize, &Lookup);
-      std::free(LBound);
-      std::free(RBound);
-      Res = Cnt % MODULO;
-    }
-    std::free(Low);
-    std::free(High);
 #ifdef DEBUG
-    std::printf("countSteppingNumbers: returning: %d\n", Res);
+  template <typename K, typename V>
+  static void printMap(std::unordered_map<K, V>* Map) {
+    for (const std::pair<K, V>& Kv : *Map) {
+      std::cout << "Key: [" << Kv.first << "], Value: [" << Kv.second << "]\n";
+    }
+  }
 #endif
-    return Res;
+
+  static int makeKey(int Start, int GapCnt) {
+    return 100 + 900 * (GapCnt >= 10) + Start * (10 + 90 * (GapCnt >= 10)) +
+           GapCnt;
   }
 
-  static int countSameLen(const char* Low, unsigned long LowSize,
-                          const char* High, unsigned long HighSize,
-                          std::unordered_map<std::string, long long>* Lookup) {
-    int Res;
-    if (LowSize == 1) {
-      Res = High[0] - Low[0] + 1;
-    } else {
-      char* Lower =
-          static_cast<char*>(std::malloc((LowSize + 1) * sizeof(char)));
-      if (Lower == nullptr) {
-        fail("countSteppingNumbers: std::malloc");
+  static void fillLookupFast(std::unordered_map<int, long long>* Lookup,
+                             int MaxGapCnt) {
+    for (int I = 0; I <= 9; ++I) {
+      long long Val = 0;
+      if (I > 0) {
+        ++Val;
       }
-      Lower[LowSize] = 0;
-      std::memcpy(Lower, Low, LowSize);
-      char* Higher =
-          static_cast<char*>(std::malloc((LowSize + 1) * sizeof(char)));
-      if (Higher == nullptr) {
-        fail("countSteppingNumbers: std::malloc");
+      if (I < 9) {
+        ++Val;
       }
-      Higher[LowSize] = 0;
-      std::memcpy(Higher, Low, LowSize);
-      long long Cnt = 0;
-      for (char I = Low[0]; I <= High[0]; ++I) {
-        Lower[0] = I;
-        Lower[1] = I - 1;
-        Higher[0] = I;
-        Higher[1] = I + 1;
-#ifdef DEBUG
-        std::printf("countSameLen: branching left\n");
-#endif
-        Cnt += countSubtree(Lower, LowSize, 1, Low, LowSize, High, HighSize,
-                            Lookup);
-#ifdef DEBUG
-        std::printf("countSameLen: branching right\n");
-#endif
-        Cnt += countSubtree(Higher, LowSize, 1, Low, LowSize, High, HighSize,
-                            Lookup);
-      }
-      std::free(Lower);
-      std::free(Higher);
-      Res = Cnt % MODULO;
+      (*Lookup)[makeKey(I, 1)] = Val;
     }
-#ifdef DEBUG
-    std::printf("countSameLen: returning: %d\n", Res);
-#endif
-    return Res;
-  }
-
-  static int countSubtree(char* Str, unsigned long StrSize,
-                          unsigned long CurIdx, const char* Low,
-                          unsigned long LowSize, const char* High,
-                          unsigned long HighSize,
-                          std::unordered_map<std::string, long long>* Lookup) {
-    int BiggerThanLow, Res;
-    unsigned long RightSize;
-    if (Str[CurIdx] < '0' || Str[CurIdx] > '9' ||
-        cmp(Str, High, CurIdx + 1) == 1 ||
-        (BiggerThanLow = cmp(Str, Low, CurIdx + 1)) == -1) {
-#ifdef DEBUG
-      std::printf("countSubtree: reached leaf node\n");
-#endif
-      Res = 0;
-    } else if ((RightSize = StrSize - CurIdx) == 1) {
-#ifdef DEBUG
-      std::printf("countSubtree: reached leaf node\n");
-#endif
-      Res = 1;
-    } else {
-      unsigned long KeySize = 2 * RightSize + 1;
-      char* Key = static_cast<char*>(std::malloc((KeySize + 1) * sizeof(char)));
-      if (Key == nullptr) {
-        fail("countSubtree: std::malloc");
-      }
-      Key[KeySize] = 0;
-      for (unsigned long I = 0; I < RightSize; ++I) {
-        Key[I] = Str[CurIdx + I];
-      }
-      Key[RightSize] = '?';
-      if (RightSize == HighSize) {
-        for (unsigned long I = 0; I < RightSize; ++I) {
-          Key[RightSize + I] = High[CurIdx + I];
+    for (int I = 2; I <= MaxGapCnt; ++I) {
+      for (int J = 0; J <= 9; ++J) {
+        long long Val = 0;
+        if (J >= 1) {
+          auto It = Lookup->find(makeKey(J - 1, I - 1));
+          Val += (*It).second;
+          Val %= MODULO;
         }
-      } else {
-        std::memset(&Key[RightSize + 1], '9', RightSize);
-      }
-      std::string KeyStr(Key);
-      auto It = Lookup->find(KeyStr);
-      if (It != Lookup->end() && cmp(Str, High, HighSize - RightSize) != 0) {
-        Res = (*It).second % MODULO;
-      } else {
-        Str[CurIdx + 1] = Str[CurIdx] - 1;
-        long long Cnt = countSubtree(Str, StrSize, CurIdx + 1, Low, LowSize,
-                                     High, HighSize, Lookup) %
-                        MODULO;
-        Str[CurIdx + 1] += 2;
-        Cnt += countSubtree(Str, StrSize, CurIdx + 1, Low, LowSize, High,
-                            HighSize, Lookup) %
-               MODULO;
-        --Str[CurIdx + 1];
-        if (BiggerThanLow != 0) {
-          (*Lookup)[KeyStr] = Cnt;
+        if (J <= 8) {
+          auto It = Lookup->find(makeKey(J + 1, I - 1));
+          Val += (*It).second;
+          Val %= MODULO;
         }
-        Res = Cnt % MODULO;
+        (*Lookup)[makeKey(J, I)] = Val;
       }
-      std::free(Key);
     }
 #ifdef DEBUG
-    std::printf("countSubtree: returning: %d\n", Res);
+    std::printf("fillLookupFast: Lookup: \n");
+    printMap<int, long long>(Lookup);
 #endif
-    return Res;
   }
 
-  static int cmp(const char* Str1, const char* Str2, unsigned long Len) {
-    for (unsigned long I = 0; I < Len; ++I) {
-      if (Str1[I] < Str2[I]) {
+  static int cmp(const std::string* Str1, const std::string* Str2,
+                 std::string::size_type Len) {
+    for (std::string::size_type I = 0; I < Len; ++I) {
+      if ((*Str1)[I] < (*Str2)[I]) {
         return -1;
-      } else if (Str1[I] > Str2[I]) {
+      } else if ((*Str1)[I] > (*Str2)[I]) {
         return 1;
       }
     }
     return 0;
+  }
+  static int countSubtree(std::string& Str, std::string::size_type CurIdx,
+                          const std::string& Low, const std::string& High,
+                          std::unordered_map<std::string, long long>* Lookup) {
+    int BiggerThanLow;
+    std::string::size_type RightSize;
+    if (Str[CurIdx] < '0' || Str[CurIdx] > '9' ||
+        cmp(&Str, &High, CurIdx + 1) == 1 ||
+        (BiggerThanLow = cmp(&Str, &Low, CurIdx + 1)) == -1) {
+      return 0;
+    } else if ((RightSize = Str.size() - CurIdx) == 1) {
+      return 1;
+    }
+    std::string Key = Str.substr(CurIdx, RightSize);
+    Key.append("?");
+    if (RightSize == High.size()) {
+      Key.append(High.substr(High.size() - RightSize, RightSize));
+    } else {
+      Key.append(std::string(RightSize, '9'));
+    }
+    auto It = Lookup->find(Key);
+    if (It != Lookup->end() && cmp(&Str, &High, High.size() - RightSize) != 0) {
+      return (*It).second % MODULO;
+    }
+    std::string Lower = Str, Higher = Str;
+    Lower[CurIdx + 1] = Str[CurIdx] - 1;
+    Higher[CurIdx + 1] = Str[CurIdx] + 1;
+    long long Res =
+        countSubtree(Lower, CurIdx + 1, Low, High, Lookup) % MODULO +
+        countSubtree(Higher, CurIdx + 1, Low, High, Lookup) % MODULO;
+    if (BiggerThanLow != 0) {
+      (*Lookup)[Key] = Res;
+    }
+    return Res % MODULO;
+  }
+
+  static int countSameLen(const std::string& low, const std::string& high,
+                          std::unordered_map<std::string, long long>* Lookup) {
+    if (low.size() == 1) {
+      return high[0] - low[0] + 1;
+    }
+    long long Cnt = 0;
+    std::string Lower = low, Higher = low;
+    for (char I = low[0]; I <= high[0]; ++I) {
+      Lower[0] = I;
+      Higher[0] = I;
+      Lower[1] = I - 1;
+      Higher[1] = I + 1;
+      if (!Lower.empty()) {
+        Cnt += countSubtree(Lower, 1, low, high, Lookup) +
+               countSubtree(Higher, 1, low, high, Lookup);
+      } else {
+        Cnt += 1;
+      }
+    }
+    return Cnt % MODULO;
+  }
+
+  static int countSameLenFast(int Len,
+                              std::unordered_map<int, long long>* Lookup) {
+    long long Cnt = 0;
+    for (int I = 1; I <= 9; ++I) {
+      auto It = Lookup->find(makeKey(I, Len - 1));
+      Cnt += It->second;
+      Cnt %= MODULO;
+    }
+    int Res = static_cast<int>(Cnt);
+#ifdef DEBUG
+    std::printf("countSameLenFast: returning %d\n", Res);
+#endif
+    return Res;
+  }
+
+  static int countSteppingNumbers(std::string low, std::string high) {
+    std::unordered_map<std::string, long long> Lookup;
+    std::unordered_map<int, long long> LookupFast;
+    if (low.size() == high.size()) {
+      return countSameLen(low, high, &Lookup) % MODULO;
+    }
+    long long Cnt = 0;
+    Cnt += countSameLen(low, std::string(low.size(), '9'), &Lookup);
+    for (std::string::size_type I = low.size() + 1; I < high.size(); ++I) {
+      if (I == low.size() + 1) {
+        fillLookupFast(&LookupFast, static_cast<int>(high.size()));
+      }
+      Cnt += countSameLenFast(static_cast<int>(I), &LookupFast);
+      Cnt %= MODULO;
+    }
+    std::string LBound = "1";
+    LBound.append(high.size() - 1, '0');
+    Cnt += countSameLen(LBound, high, &Lookup);
+    return Cnt % MODULO;
   }
 };
--- count_stepping_numbers/Test.cpp
+++ count_stepping_numbers/Test.cpp
@@ -3,90 +3,13 @@
 
 #include "Main.cpp"
 
-static void testCmp() {
-  std::string Str1 = "123", Str2 = "124";
-  assert(Solution::cmp(Str1.c_str(), Str2.c_str(), 3) == -1);
-  Str1 = "124";
-  Str2 = "123";
-  assert(Solution::cmp(Str1.c_str(), Str2.c_str(), 3) == 1);
-  Str1 = "123";
-  Str2 = "123";
-  assert(Solution::cmp(Str1.c_str(), Str2.c_str(), 3) == 0);
-}
-
-// static void testCountSubtree() {
-//   {
-//     std::unordered_map<std::string, long long> Lookup;
-//     std::string Low = "10", High = "19";
-//     assert(Solution::countSubtree(&Low, 0, Low, High, &Lookup) == 2);
-//   }
-//   {
-//     std::unordered_map<std::string, long long> Lookup;
-//     std::string Low = "100", High = "200";
-//     assert(Solution::countSubtree(&Low, 0, Low, High, &Lookup) == 3);
-//   }
-//   {
-//     std::unordered_map<std::string, long long> Lookup;
-//     std::string Low = "2023", High = "2999";
-//     assert(Solution::countSubtree(&Low, 0, Low, High, &Lookup) == 7);
-//   }
-//   {
-//     std::unordered_map<std::string, long long> Lookup;
-//     std::string Low = "100000", High = "199999";
-//     assert(Solution::countSubtree(&Low, 0, Low, High, &Lookup) == 20);
-//   }
-//   {
-//     std::unordered_map<std::string, long long> Lookup;
-//     std::string Low = "100000", High = "120000";
-//     assert(Solution::countSubtree(&Low, 0, Low, High, &Lookup) == 6);
-//   }
-//   {
-//     std::unordered_map<std::string, long long> Lookup;
-//     std::string Low = "120000", High = "199999";
-//     assert(Solution::countSubtree(&Low, 0, Low, High, &Lookup) == 14);
-//   }
-//   {
-//     std::unordered_map<std::string, long long> Lookup;
-//     std::string Low = "33000000", High = "37000000";
-//     assert(Solution::countSubtree(&Low, 0, Low, High, &Lookup) == 61);
-//   }
-//   {
-//     std::unordered_map<std::string, long long> Lookup;
-//     std::string Low = "106", High = "121";
-//     assert(Solution::countSubtree(&Low, 0, Low, High, &Lookup) == 1);
-//   }
-// }
-
-static void testCountSameLen() {
-  std::unordered_map<std::string, long long> Lookup;
-  std::string Low = "33000000", High = "37000000";
-  assert(Solution::countSameLen(Low.c_str(), Low.size(), High.c_str(),
-                                High.size(), &Lookup) == 61);
-  Low = "3000000";
-  High = "7000000";
-  assert(Solution::countSameLen(Low.c_str(), Low.size(), High.c_str(),
-                                High.size(), &Lookup) == 234);
-  Low = "1000000";
-  High = "8999999";
-  assert(Solution::countSameLen(Low.c_str(), Low.size(), High.c_str(),
-                                High.size(), &Lookup) == 404);
-  Low = "1";
-  High = "9";
-  assert(Solution::countSameLen(Low.c_str(), Low.size(), High.c_str(),
-                                High.size(), &Lookup) == 9);
-  Low = "100";
-  High = "999";
-  assert(Solution::countSameLen(Low.c_str(), Low.size(), High.c_str(),
-                                High.size(), &Lookup) == 32);
-}
-
 static void testCountSteppingNumbers() {
-  assert(Solution::countSteppingNumbers("100", "999") == 32);
-  assert(Solution::countSteppingNumbers("1", "11") == 10);
-  assert(Solution::countSteppingNumbers("90", "101") == 2);
-  assert(Solution::countSteppingNumbers("1", "10000000") == 881);
-  assert(Solution::countSteppingNumbers("96", "322") == 9);
-  assert(Solution::countSteppingNumbers("129", "439") == 10);
+  // assert(Solution::countSteppingNumbers("100", "999") == 32);
+  // assert(Solution::countSteppingNumbers("1", "11") == 10);
+  // assert(Solution::countSteppingNumbers("90", "101") == 2);
+  // assert(Solution::countSteppingNumbers("1", "10000000") == 881);
+  // assert(Solution::countSteppingNumbers("96", "322") == 9);
+  // assert(Solution::countSteppingNumbers("129", "439") == 10);
   assert(Solution::countSteppingNumbers(
              "1",
              "999999999999999999999999999999999999999999999999999999999999999"
@@ -94,7 +17,7 @@ static void testCountSteppingNumbers() {
 }
 
 static void profCountSteppingNumbers() {
-  int RunCnt = 100;
+  int RunCnt = 500;
   double Dur = 0;
   for (int I = 0; I < RunCnt; ++I) {
     auto Begin = std::chrono::system_clock::now();
@@ -111,9 +34,6 @@ static void profCountSteppingNumbers() {
 }
 
 int main() {
-  // testCmp();
-  // testCountSubtree();
-  // testCountSameLen();
   testCountSteppingNumbers();
   profCountSteppingNumbers();
   return 0;
