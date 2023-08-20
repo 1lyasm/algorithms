605735869214cca02c6192686532ab3f63601569
1lyasm <il.mystafa@gmail.com>
2023-08-20 21:10:57 +0400

    Use long long** for Table

--- count_stepping_numbers/Main.cpp
+++ count_stepping_numbers/Main.cpp
@@ -1,28 +1,31 @@
-#include <iostream>
 #include <string>
 #include <vector>
 
-#define DEBUG
+// #define DEBUG
+
+#ifdef DEBUG
+#include <iostream>
+#define debug(x) x
+#else
+#define debug(x)
+#endif
+
 #define cnt countSteppingNumbers
 #define MOD static_cast<int>(1e9 + 7)
 
 class Solution {
-#ifdef DEBUG
-  template <typename T>
-  static void printTable(T Table) {
+  debug(template <typename T> static void printTable(T Table) {
     for (const auto &R : Table) {
       for (const auto &V : R) {
         std::cout << V << " ";
       }
       std::cout << "\n";
     }
-  }
-#endif
+  });
+
  public:
-  static std::vector<std::vector<long long>> makeTable(int Mod) {
-    std::size_t RowCnt = 10, ColCnt = 100;
-    std::vector<std::vector<long long>> Table(RowCnt,
-                                              std::vector<long long>(ColCnt));
+  static void fillTable(long long **Table, std::size_t RowCnt,
+                        std::size_t ColCnt) {
     for (std::size_t I = 0; I < RowCnt; ++I) {
       Table[I][0] = 1;
     }
@@ -30,36 +33,97 @@ class Solution {
       Table[0][J] = Table[1][J - 1];
       for (std::size_t I = 1; I < RowCnt - 1; ++I) {
         Table[I][J] = Table[I - 1][J - 1] + Table[I + 1][J - 1];
-        Table[I][J] %= Mod;
+        Table[I][J] %= MOD;
       }
       Table[RowCnt - 1][J] = Table[RowCnt - 2][J - 1];
     }
-    return Table;
   }
 
-  static long long pruneLeft(const std::string &low,
-                             const std::vector<std::vector<long long>> &Table) {
-    if (low.size() == 1) {
-      return low[0] - '0' - 1;
-    }
+  static long long pruneL(const std::string &low, long long **Table) {
     long long PrunedCnt = 0;
-    for (std::size_t I = 1; I < low.size(); ++I) {
-      if (low[I] >= low[I - 1] && low[I - 1] != '0') {
-        PrunedCnt += Table[low[I - 1] - '0' - 1][low.size() - I - 1];
-      }
-      if (low[I] > low[I - 1] + 1) {
-        PrunedCnt += Table[low[I - 1] - '0' + 1][low.size() - I - 1];
-      }
-      if (low[I] < low[I - 1] - 1 || low[I] == low[I - 1]) {
-        break;
+    if (low.size() != 1) {
+      for (std::size_t I = 1; I < low.size(); ++I) {
+        if (low[I] >= low[I - 1] && low[I - 1] != '0') {
+          PrunedCnt += Table[static_cast<std::size_t>(low[I - 1] - '0') - 1]
+                            [low.size() - I - 1];
+        }
+        if (low[I] > low[I - 1] + 1) {
+          PrunedCnt += Table[static_cast<std::size_t>(low[I - 1] - '0') + 1]
+                            [low.size() - I - 1];
+        }
+        if (low[I] < low[I - 1] - 1 || low[I] == low[I - 1] ||
+            low[I] > low[I - 1] + 1) {
+          break;
+        }
       }
     }
+    debug(std::cout << "pruneL: returning: " << PrunedCnt << "\n");
     return PrunedCnt;
   }
 
+  static long long pruneR(const std::string &high, long long **Table) {
+    long long PrunedCnt = 0;
+    if (high.size() != 1) {
+      for (std::size_t I = 1; I < high.size(); ++I) {
+        if (high[I] <= high[I - 1] && high[I - 1] != '9') {
+          PrunedCnt += Table[static_cast<std::size_t>(high[I - 1] - '0') + 1]
+                            [high.size() - I - 1];
+        }
+        if (high[I] < high[I - 1] - 1) {
+          PrunedCnt += Table[static_cast<std::size_t>(high[I - 1] - '0') - 1]
+                            [high.size() - I - 1];
+        }
+        if (high[I] == high[I - 1] || high[I] > high[I - 1] + 1 ||
+            high[I] < high[I - 1] - 1) {
+          break;
+        }
+      }
+    }
+    debug(std::cout << "pruneR: returning: " << PrunedCnt << "\n");
+    return PrunedCnt;
+  }
+
+  static long long cntRaw(std::string low, std::string high, long long **Table,
+                          std::size_t RowCnt) {
+    long long Cnt = 0;
+    std::size_t LowFirst = static_cast<std::size_t>(low[0] - '0'),
+                HighFirst = static_cast<std::size_t>(high[0] - '0');
+    if (low.size() == high.size()) {
+      for (std::size_t I = LowFirst; I <= HighFirst; ++I) {
+        Cnt += Table[I][low.size() - 1];
+      }
+    } else {
+      for (std::size_t I = LowFirst; I < RowCnt; ++I) {
+        Cnt += Table[I][low.size() - 1];
+      }
+      for (std::size_t J = low.size(); J < high.size() - 1; ++J) {
+        for (std::size_t I = 1; I < RowCnt; ++I) {
+          Cnt += Table[I][J];
+        }
+      }
+      for (std::size_t I = 1; I <= HighFirst; ++I) {
+        Cnt += Table[I][high.size() - 1];
+      }
+    }
+    debug(std::cout << "cntRaw: returning: " << Cnt << "\n");
+    return Cnt;
+  }
+
   static int cnt(std::string low, std::string high) {
-    auto Table = makeTable(MOD);
-    long long PrunedCnt = pruneLeft(low, Table);
-    return 0;
+    const unsigned long RowCnt = 10, ColCnt = std::max(low.size(), high.size());
+    long long **Table = new long long *[RowCnt];
+    for (unsigned long I = 0; I < RowCnt; ++I) {
+      Table[I] = new long long[ColCnt];
+    }
+    fillTable(Table, RowCnt, ColCnt);
+    int Res = (cntRaw(low, high, Table, RowCnt) + 2 * MOD - pruneL(low, Table) -
+               pruneR(high, Table)) %
+              MOD;
+    for (unsigned long I = 0; I < RowCnt; ++I) {
+      delete[] Table[I];
+    }
+    delete[] Table;
+    debug(std::cout << "cnt: returning: " << Res << "\n");
+    return Res;
   }
 };
--- count_stepping_numbers/Test.cpp
+++ count_stepping_numbers/Test.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <chrono>
+#include <iostream>
 
 #include "Main.cpp"
 
@@ -7,67 +8,148 @@ static void pass(std::string FuncName) {
   std::cout << FuncName << ": passed\n";
 }
 
-static void testPruneLeft() {
-  auto Table = Solution::makeTable(MOD);
-  assert(Solution::pruneLeft("100", Table) == 0);
-  assert(Solution::pruneLeft("101", Table) == 0);
-  assert(Solution::pruneLeft("102", Table) == 1);
-  assert(Solution::pruneLeft("103", Table) == 1);
-  assert(Solution::pruneLeft("109", Table) == 1);
-  assert(Solution::pruneLeft("110", Table) == 1);
-  assert(Solution::pruneLeft("111", Table) == 1);
-  assert(Solution::pruneLeft("112", Table) == 1);
-  assert(Solution::pruneLeft("113", Table) == 1);
-  assert(Solution::pruneLeft("114", Table) == 1);
-  assert(Solution::pruneLeft("119", Table) == 1);
-  assert(Solution::pruneLeft("120", Table) == 1);
-  assert(Solution::pruneLeft("121", Table) == 1);
-  assert(Solution::pruneLeft("122", Table) == 2);
-  assert(Solution::pruneLeft("123", Table) == 2);
-  assert(Solution::pruneLeft("124", Table) == 3);
-  assert(Solution::pruneLeft("125", Table) == 3);
-  assert(Solution::pruneLeft("129", Table) == 3);
-  assert(Solution::pruneLeft("130", Table) == 3);
-  assert(Solution::pruneLeft("198", Table) == 3);
-  assert(Solution::pruneLeft("10", Table) == 0);
-  assert(Solution::pruneLeft("11", Table) == 1);
-  assert(Solution::pruneLeft("5", Table) == 4);
-  assert(Solution::pruneLeft("1", Table) == 0);
-  assert(Solution::pruneLeft("9", Table) == 8);
-  assert(Solution::pruneLeft("12", Table) == 1);
-  assert(Solution::pruneLeft("13", Table) == 2);
-  assert(Solution::pruneLeft("14", Table) == 2);
-  assert(Solution::pruneLeft("19", Table) == 2);
-  assert(Solution::pruneLeft("201", Table) == 0);
-  assert(Solution::pruneLeft("210", Table) == 0);
-  assert(Solution::pruneLeft("211", Table) == 1);
-  assert(Solution::pruneLeft("212", Table) == 1);
-  assert(Solution::pruneLeft("213", Table) == 2);
-  assert(Solution::pruneLeft("215", Table) == 2);
-  assert(Solution::pruneLeft("220", Table) == 2);
-  assert(Solution::pruneLeft("221", Table) == 2);
-  assert(Solution::pruneLeft("222", Table) == 2);
-  assert(Solution::pruneLeft("230", Table) == 2);
-  assert(Solution::pruneLeft("232", Table) == 2);
-  assert(Solution::pruneLeft("233", Table) == 3);
-  assert(Solution::pruneLeft("234", Table) == 3);
-  assert(Solution::pruneLeft("235", Table) == 4);
-  assert(Solution::pruneLeft("100000", Table) == 0);
-  assert(Solution::pruneLeft("987654321", Table) == 0);
-  pass("testPruneLeft");
-}
+// static void testPruneL() {
+//   auto Table = Solution::makeTable(MOD, 100);
+//   assert(Solution::pruneL("100", Table) == 0);
+//   assert(Solution::pruneL("101", Table) == 0);
+//   assert(Solution::pruneL("102", Table) == 1);
+//   assert(Solution::pruneL("103", Table) == 1);
+//   assert(Solution::pruneL("109", Table) == 1);
+//   assert(Solution::pruneL("110", Table) == 1);
+//   assert(Solution::pruneL("111", Table) == 1);
+//   assert(Solution::pruneL("112", Table) == 1);
+//   assert(Solution::pruneL("113", Table) == 1);
+//   assert(Solution::pruneL("114", Table) == 1);
+//   assert(Solution::pruneL("119", Table) == 1);
+//   assert(Solution::pruneL("120", Table) == 1);
+//   assert(Solution::pruneL("121", Table) == 1);
+//   assert(Solution::pruneL("122", Table) == 2);
+//   assert(Solution::pruneL("123", Table) == 2);
+//   assert(Solution::pruneL("124", Table) == 3);
+//   assert(Solution::pruneL("125", Table) == 3);
+//   assert(Solution::pruneL("129", Table) == 3);
+//   assert(Solution::pruneL("130", Table) == 3);
+//   assert(Solution::pruneL("198", Table) == 3);
+//   assert(Solution::pruneL("10", Table) == 0);
+//   assert(Solution::pruneL("11", Table) == 1);
+//   assert(Solution::pruneL("5", Table) == 0);
+//   assert(Solution::pruneL("1", Table) == 0);
+//   assert(Solution::pruneL("9", Table) == 0);
+//   assert(Solution::pruneL("12", Table) == 1);
+//   assert(Solution::pruneL("13", Table) == 2);
+//   assert(Solution::pruneL("14", Table) == 2);
+//   assert(Solution::pruneL("19", Table) == 2);
+//   assert(Solution::pruneL("201", Table) == 0);
+//   assert(Solution::pruneL("210", Table) == 0);
+//   assert(Solution::pruneL("211", Table) == 1);
+//   assert(Solution::pruneL("212", Table) == 1);
+//   assert(Solution::pruneL("213", Table) == 2);
+//   assert(Solution::pruneL("215", Table) == 2);
+//   assert(Solution::pruneL("220", Table) == 2);
+//   assert(Solution::pruneL("221", Table) == 2);
+//   assert(Solution::pruneL("222", Table) == 2);
+//   assert(Solution::pruneL("230", Table) == 2);
+//   assert(Solution::pruneL("232", Table) == 2);
+//   assert(Solution::pruneL("233", Table) == 3);
+//   assert(Solution::pruneL("234", Table) == 3);
+//   assert(Solution::pruneL("235", Table) == 4);
+//   assert(Solution::pruneL("100000", Table) == 0);
+//   assert(Solution::pruneL("987654321", Table) == 0);
+//   pass("testPruneL");
+// }
+
+// static void testPruneR() {
+//   auto Table = Solution::makeTable(MOD, 100);
+//   assert(Solution::pruneR("100", Table) == 3);
+//   assert(Solution::pruneR("101", Table) == 2);
+//   assert(Solution::pruneR("102", Table) == 2);
+//   assert(Solution::pruneR("103", Table) == 2);
+//   assert(Solution::pruneR("109", Table) == 2);
+//   assert(Solution::pruneR("110", Table) == 2);
+//   assert(Solution::pruneR("111", Table) == 2);
+//   assert(Solution::pruneR("112", Table) == 2);
+//   assert(Solution::pruneR("113", Table) == 2);
+//   assert(Solution::pruneR("114", Table) == 2);
+//   assert(Solution::pruneR("119", Table) == 2);
+//   assert(Solution::pruneR("120", Table) == 2);
+//   assert(Solution::pruneR("121", Table) == 1);
+//   assert(Solution::pruneR("122", Table) == 1);
+//   assert(Solution::pruneR("123", Table) == 0);
+//   assert(Solution::pruneR("124", Table) == 0);
+//   assert(Solution::pruneR("125", Table) == 0);
+//   assert(Solution::pruneR("129", Table) == 0);
+//   assert(Solution::pruneR("130", Table) == 0);
+//   assert(Solution::pruneR("198", Table) == 0);
+//   assert(Solution::pruneR("10", Table) == 1);
+//   assert(Solution::pruneR("11", Table) == 1);
+//   assert(Solution::pruneR("5", Table) == 0);
+//   assert(Solution::pruneR("1", Table) == 0);
+//   assert(Solution::pruneR("9", Table) == 0);
+//   assert(Solution::pruneR("12", Table) == 0);
+//   assert(Solution::pruneR("13", Table) == 0);
+//   assert(Solution::pruneR("14", Table) == 0);
+//   assert(Solution::pruneR("19", Table) == 0);
+//   assert(Solution::pruneR("201", Table) == 4);
+//   assert(Solution::pruneR("210", Table) == 3);
+//   assert(Solution::pruneR("211", Table) == 3);
+//   assert(Solution::pruneR("212", Table) == 2);
+//   assert(Solution::pruneR("213", Table) == 2);
+//   assert(Solution::pruneR("215", Table) == 2);
+//   assert(Solution::pruneR("220", Table) == 2);
+//   assert(Solution::pruneR("221", Table) == 2);
+//   assert(Solution::pruneR("222", Table) == 2);
+//   assert(Solution::pruneR("230", Table) == 2);
+//   assert(Solution::pruneR("232", Table) == 1);
+//   assert(Solution::pruneR("233", Table) == 1);
+//   assert(Solution::pruneR("234", Table) == 0);
+//   assert(Solution::pruneR("235", Table) == 0);
+//   assert(Solution::pruneR("99999", Table) == 0);
+//   assert(Solution::pruneR("123456789", Table) == 0);
+//   assert(Solution::pruneR("200", Table) == 4);
+//   pass("testPruneR");
+// }
+
+// static void testCntRaw() {
+//   auto Table = Solution::makeTable(MOD, 100);
+//   assert(Solution::cntRaw("100", "999", Table) == 32);
+//   assert(Solution::cntRaw("1", "9", Table) == 9);
+//   assert(Solution::cntRaw("1", "99", Table) == 26);
+//   assert(Solution::cntRaw("1", "999", Table) == 58);
+//   assert(Solution::cntRaw("30", "99", Table) == 13);
+//   assert(Solution::cntRaw("30", "79", Table) == 10);
+//   assert(Solution::cntRaw("1", "49", Table) == 17);
+//   assert(Solution::cntRaw(
+//              "1",
+//              "999999999999999999999999999999999999999999999999999999999999999"
+//              "9999999999999999999999999999999999999",
+//              Table) == 125046265);
+//   pass("testCntRaw");
+// }
 
 static void testCnt() {
-  assert(Solution::countSteppingNumbers("100", "999") == 32);
-  assert(Solution::countSteppingNumbers("1", "11") == 10);
-  assert(Solution::countSteppingNumbers("90", "101") == 2);
-  assert(Solution::countSteppingNumbers("1", "10000000") == 881);
-  assert(Solution::countSteppingNumbers("96", "322") == 9);
-  assert(Solution::countSteppingNumbers("129", "439") == 10);
-  assert(Solution::countSteppingNumbers(
+  assert(Solution::cnt("100", "999") == 32);
+  assert(Solution::cnt("1", "11") == 10);
+  assert(Solution::cnt("90", "101") == 2);
+  assert(Solution::cnt("1", "10000000") == 881);
+  assert(Solution::cnt("96", "322") == 9);
+  assert(Solution::cnt("129", "439") == 10);
+  assert(Solution::cnt("2", "40") == 14);
+  assert(Solution::cnt("99", "200") == 3);
+  assert(Solution::cnt("149", "160") == 0);
+  assert(Solution::cnt("1", "1") == 1);
+  assert(Solution::cnt("9999999999999999999999999999999999999999999999999999999"
+                       "99999999999999999999999999999999999",
+                       "9999999999999999999999999999999999999999999999999999999"
+                       "999999999999999999999999999999999999") == 705298990);
+  assert(Solution::cnt(
              "1",
              "999999999999999999999999999999999999999999999999999999999999999"
              "9999999999999999999999999999999999999") == 125046265);
+  assert(Solution::cnt("4777151832123893594603912184102647750886581638454062882"
+                       "3553703379973973374976021909186485563776056",
+                       "5267144055329453476845598490077325117111896155350045515"
+                       "152421482060338688466687508314627537686720152") ==
+         683315344);
   pass("testCnt");
 }
 
@@ -76,7 +158,7 @@ static void profCnt() {
   double Dur = 0;
   for (int I = 0; I < RunCnt; ++I) {
     auto Begin = std::chrono::system_clock::now();
-    assert(Solution::countSteppingNumbers(
+    assert(Solution::cnt(
                "1",
                "999999999999999999999999999999999999999999999999999999999999999"
                "9999999999999999999999999999999999999") == 125046265);
@@ -89,7 +171,9 @@ static void profCnt() {
 }
 
 int main() {
-  testPruneLeft();
+  // testPruneL();
+  // testPruneR();
+  // testCntRaw();
   testCnt();
   profCnt();
   return 0;
