3f8f06428d546547d9232afc6ebcd1f1f1807a68
1lyasm <il.mystafa@gmail.com>
2023-08-20 17:37:56 +0400

    Make pruneLeft shorter

--- count_stepping_numbers/Main.cpp
+++ count_stepping_numbers/Main.cpp
@@ -1,162 +1,65 @@
-#include <cassert>
-#include <chrono>
-#include <climits>
 #include <iostream>
-#include <map>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
-// #define DEBUG
-
-#define MODULO (1000000000 + 7)
+#define DEBUG
+#define cnt countSteppingNumbers
+#define MOD static_cast<int>(1e9 + 7)
 
 class Solution {
+#ifdef DEBUG
+  template <typename T>
+  static void printTable(T Table) {
+    for (const auto &R : Table) {
+      for (const auto &V : R) {
+        std::cout << V << " ";
+      }
+      std::cout << "\n";
+    }
+  }
+#endif
  public:
-#ifdef DEBUG
-  template <typename K, typename V>
-  static void printMap(std::unordered_map<K, V>* Map) {
-    for (const std::pair<K, V>& Kv : *Map) {
-      std::cout << "Key: [" << Kv.first << "], Value: [" << Kv.second << "]\n";
+  static std::vector<std::vector<long long>> makeTable(int Mod) {
+    std::size_t RowCnt = 10, ColCnt = 100;
+    std::vector<std::vector<long long>> Table(RowCnt,
+                                              std::vector<long long>(ColCnt));
+    for (std::size_t I = 0; I < RowCnt; ++I) {
+      Table[I][0] = 1;
     }
-  }
-#endif
-
-  static void fillLookupFast(std::vector<std::vector<long long>>* Lookup,
-                             unsigned long MaxGapCnt) {
-    for (unsigned long I = 0; I <= 9; ++I) {
-      long long Val = 0;
-      if (I > 0) {
-        ++Val;
+    for (std::size_t J = 1; J < ColCnt; ++J) {
+      Table[0][J] = Table[1][J - 1];
+      for (std::size_t I = 1; I < RowCnt - 1; ++I) {
+        Table[I][J] = Table[I - 1][J - 1] + Table[I + 1][J - 1];
+        Table[I][J] %= Mod;
       }
-      if (I < 9) {
-        ++Val;
-      }
-      (*Lookup)[I][1] = Val;
+      Table[RowCnt - 1][J] = Table[RowCnt - 2][J - 1];
     }
-    for (unsigned long I = 2; I <= MaxGapCnt; ++I) {
-      for (unsigned long J = 0; J <= 9; ++J) {
-        long long Val = 0;
-        if (J >= 1) {
-          Val += (*Lookup)[J - 1][I - 1];
-          Val %= MODULO;
-        }
-        if (J <= 8) {
-          Val += (*Lookup)[J + 1][I - 1];
-          Val %= MODULO;
-        }
-        (*Lookup)[J][I] = Val;
-      }
-    }
-#ifdef DEBUG
-    std::printf("fillLookupFast: Lookup: \n");
-    printMap<int, long long>(Lookup);
-#endif
+    return Table;
   }
 
-  static int cmp(const char* Str1, const char* Str2,
-                 std::string::size_type Len) {
-    for (std::string::size_type I = 0; I < Len; ++I) {
-      if (Str1[I] < Str2[I]) {
-        return -1;
-      } else if (Str1[I] > Str2[I]) {
-        return 1;
-      }
-    }
-    return 0;
-  }
-
-  static int countSubtree(std::string& Str, std::string::size_type CurIdx,
-                          const std::string& Low, const std::string& High,
-                          std::unordered_map<std::string, long long>* Lookup) {
-    int BiggerThanLow;
-    std::string::size_type RightSize;
-    if (Str[CurIdx] < '0' || Str[CurIdx] > '9' ||
-        cmp(Str.c_str(), High.c_str(), CurIdx + 1) == 1 ||
-        (BiggerThanLow = cmp(Str.c_str(), Low.c_str(), CurIdx + 1)) == -1) {
-      return 0;
-    } else if ((RightSize = Str.size() - CurIdx) == 1) {
-      return 1;
-    }
-    std::string Key = Str.substr(CurIdx, RightSize);
-    Key.append("?");
-    if (RightSize == High.size()) {
-      Key.append(High.substr(High.size() - RightSize, RightSize));
-    } else {
-      Key.append(std::string(RightSize, '9'));
-    }
-    auto It = Lookup->find(Key);
-    if (It != Lookup->end() &&
-        cmp(Str.c_str(), High.c_str(), High.size() - RightSize) != 0) {
-      return It->second % MODULO;
-    }
-    std::string Lower = Str, Higher = Str;
-    Lower[CurIdx + 1] = Str[CurIdx] - 1;
-    Higher[CurIdx + 1] = Str[CurIdx] + 1;
-    long long Res =
-        countSubtree(Lower, CurIdx + 1, Low, High, Lookup) % MODULO +
-        countSubtree(Higher, CurIdx + 1, Low, High, Lookup) % MODULO;
-    if (BiggerThanLow != 0) {
-      (*Lookup)[Key] = Res;
-    }
-    return Res % MODULO;
-  }
-
-  static int countSameLen(const std::string& low, const std::string& high,
-                          std::unordered_map<std::string, long long>* Lookup) {
+  static long long pruneLeft(const std::string &low,
+                             const std::vector<std::vector<long long>> &Table) {
     if (low.size() == 1) {
-      return high[0] - low[0] + 1;
+      return low[0] - '0' - 1;
     }
-    long long Cnt = 0;
-    std::string Lower = low, Higher = low;
-    for (char I = low[0]; I <= high[0]; ++I) {
-      Lower[0] = I;
-      Higher[0] = I;
-      Lower[1] = I - 1;
-      Higher[1] = I + 1;
-      if (!Lower.empty()) {
-        Cnt += countSubtree(Lower, 1, low, high, Lookup) +
-               countSubtree(Higher, 1, low, high, Lookup);
-      } else {
-        Cnt += 1;
+    long long PrunedCnt = 0;
+    for (std::size_t I = 1; I < low.size(); ++I) {
+      if (low[I] >= low[I - 1] && low[I - 1] != '0') {
+        PrunedCnt += Table[low[I - 1] - '0' - 1][low.size() - I - 1];
+      }
+      if (low[I] > low[I - 1] + 1) {
+        PrunedCnt += Table[low[I - 1] - '0' + 1][low.size() - I - 1];
+      }
+      if (low[I] < low[I - 1] - 1 || low[I] == low[I - 1]) {
+        break;
       }
     }
-    return Cnt % MODULO;
+    return PrunedCnt;
   }
 
-  static int countSameLenFast(unsigned long Len,
-                              std::vector<std::vector<long long>>* Lookup) {
-    long long Cnt = 0;
-    for (unsigned long I = 1; I <= 9; ++I) {
-      Cnt += (*Lookup)[I][Len - 1];
-      Cnt %= MODULO;
-    }
-    int Res = static_cast<int>(Cnt);
-#ifdef DEBUG
-    std::printf("countSameLenFast: returning %d\n", Res);
-#endif
-    return Res;
-  }
-
-  static int countSteppingNumbers(std::string low, std::string high) {
-    std::unordered_map<std::string, long long> Lookup;
-    std::vector<std::vector<long long>> LookupFast(10,
-                                                   std::vector<long long>(100));
-    if (low.size() == high.size()) {
-      return countSameLen(low, high, &Lookup) % MODULO;
-    }
-    long long Cnt = 0;
-    Cnt += countSameLen(low, std::string(low.size(), '9'), &Lookup);
-    for (std::string::size_type I = low.size() + 1; I < high.size(); ++I) {
-      if (I == low.size() + 1) {
-        fillLookupFast(&LookupFast, high.size() - 1);
-      }
-      Cnt += countSameLenFast(I, &LookupFast);
-      Cnt %= MODULO;
-    }
-    std::string LBound = "1";
-    LBound.append(high.size() - 1, '0');
-    Cnt += countSameLen(LBound, high, &Lookup);
-    return Cnt % MODULO;
+  static int cnt(std::string low, std::string high) {
+    auto Table = makeTable(MOD);
+    long long PrunedCnt = pruneLeft(low, Table);
+    return 0;
   }
 };
--- count_stepping_numbers/Test.cpp
+++ count_stepping_numbers/Test.cpp
@@ -3,7 +3,61 @@
 
 #include "Main.cpp"
 
-static void testCountSteppingNumbers() {
+static void pass(std::string FuncName) {
+  std::cout << FuncName << ": passed\n";
+}
+
+static void testPruneLeft() {
+  auto Table = Solution::makeTable(MOD);
+  assert(Solution::pruneLeft("100", Table) == 0);
+  assert(Solution::pruneLeft("101", Table) == 0);
+  assert(Solution::pruneLeft("102", Table) == 1);
+  assert(Solution::pruneLeft("103", Table) == 1);
+  assert(Solution::pruneLeft("109", Table) == 1);
+  assert(Solution::pruneLeft("110", Table) == 1);
+  assert(Solution::pruneLeft("111", Table) == 1);
+  assert(Solution::pruneLeft("112", Table) == 1);
+  assert(Solution::pruneLeft("113", Table) == 1);
+  assert(Solution::pruneLeft("114", Table) == 1);
+  assert(Solution::pruneLeft("119", Table) == 1);
+  assert(Solution::pruneLeft("120", Table) == 1);
+  assert(Solution::pruneLeft("121", Table) == 1);
+  assert(Solution::pruneLeft("122", Table) == 2);
+  assert(Solution::pruneLeft("123", Table) == 2);
+  assert(Solution::pruneLeft("124", Table) == 3);
+  assert(Solution::pruneLeft("125", Table) == 3);
+  assert(Solution::pruneLeft("129", Table) == 3);
+  assert(Solution::pruneLeft("130", Table) == 3);
+  assert(Solution::pruneLeft("198", Table) == 3);
+  assert(Solution::pruneLeft("10", Table) == 0);
+  assert(Solution::pruneLeft("11", Table) == 1);
+  assert(Solution::pruneLeft("5", Table) == 4);
+  assert(Solution::pruneLeft("1", Table) == 0);
+  assert(Solution::pruneLeft("9", Table) == 8);
+  assert(Solution::pruneLeft("12", Table) == 1);
+  assert(Solution::pruneLeft("13", Table) == 2);
+  assert(Solution::pruneLeft("14", Table) == 2);
+  assert(Solution::pruneLeft("19", Table) == 2);
+  assert(Solution::pruneLeft("201", Table) == 0);
+  assert(Solution::pruneLeft("210", Table) == 0);
+  assert(Solution::pruneLeft("211", Table) == 1);
+  assert(Solution::pruneLeft("212", Table) == 1);
+  assert(Solution::pruneLeft("213", Table) == 2);
+  assert(Solution::pruneLeft("215", Table) == 2);
+  assert(Solution::pruneLeft("220", Table) == 2);
+  assert(Solution::pruneLeft("221", Table) == 2);
+  assert(Solution::pruneLeft("222", Table) == 2);
+  assert(Solution::pruneLeft("230", Table) == 2);
+  assert(Solution::pruneLeft("232", Table) == 2);
+  assert(Solution::pruneLeft("233", Table) == 3);
+  assert(Solution::pruneLeft("234", Table) == 3);
+  assert(Solution::pruneLeft("235", Table) == 4);
+  assert(Solution::pruneLeft("100000", Table) == 0);
+  assert(Solution::pruneLeft("987654321", Table) == 0);
+  pass("testPruneLeft");
+}
+
+static void testCnt() {
   assert(Solution::countSteppingNumbers("100", "999") == 32);
   assert(Solution::countSteppingNumbers("1", "11") == 10);
   assert(Solution::countSteppingNumbers("90", "101") == 2);
@@ -14,9 +68,10 @@ static void testCountSteppingNumbers() {
              "1",
              "999999999999999999999999999999999999999999999999999999999999999"
              "9999999999999999999999999999999999999") == 125046265);
+  pass("testCnt");
 }
 
-static void profCountSteppingNumbers() {
+static void profCnt() {
   int RunCnt = 500;
   double Dur = 0;
   for (int I = 0; I < RunCnt; ++I) {
@@ -30,11 +85,12 @@ static void profCountSteppingNumbers() {
     Dur += Diff.count();
   }
   double AvgDur = Dur / RunCnt;
-  std::printf("profCountSteppingNumbers: duration: %f\n", AvgDur);
+  std::printf("profCnt: duration: %f\n", AvgDur);
 }
 
 int main() {
-  testCountSteppingNumbers();
-  profCountSteppingNumbers();
+  testPruneLeft();
+  testCnt();
+  profCnt();
   return 0;
 }
