59680b7e7468fa6f3d32e1d9b28497344cebb65a
1lyasm <il.mystafa@gmail.com>
2023-08-21 20:17:36 +0400

    Simplify

--- minimum_cost_split_array/Main.cpp
+++ minimum_cost_split_array/Main.cpp
@@ -1,44 +1,20 @@
 class Solution {
-public:
-    int compVal(vector<int> &nums, int start, int end, int k) {
-        int uniqueCnt = 0;
-        for (int i = start; i <= end; ++i) {
-            bool isUnique = true;
-            for (int j = start; j <= end; ++j) {
-                if (nums[i] == nums[j] && i != j) {
-                    isUnique = false;
-                    break;
-                }
-            }
-            if (isUnique) {
-                ++uniqueCnt;
-            }
-        }
-        return end - start - uniqueCnt + k;
+ public:
+  int minCost(vector<int>& nums, int k) {
+    int n = nums.size();
+    vector<int> costs(n + 1, INT_MAX / 2);
+    costs[n] = 0;
+    for (int i = n - 1; i >= 0; --i) {
+      int len = 0;
+      vector<int> counts(n);
+      for (int j = i; j < n; ++j) {
+        if (++counts[nums[j]] == 2)
+          len += 2;
+        else if (counts[nums[j]] > 2)
+          ++len;
+        costs[i] = min(costs[i], len + k + costs[j + 1]);
+      }
     }
-
-    int solve(vector<int> &nums, int start, int end, int k) {
-        if (end < start) {
-            return 0;
-        }
-        if (end == start) {
-            return k;
-        }
-        std::cout << "solve: start: " << start << ", end: " << end << "\n";
-        int min = INT_MAX;
-        for (int i = start; i < end; ++i) {
-            int cost = solve(nums, start, i, k) + 
-                solve(nums, i + 1, end, k);
-            cost += compVal(nums, start, i, k) + 
-                compVal(nums, i + 1, end, k);
-            if (cost < min) {
-                min = cost;
-            }
-        }
-        return min;
-    }
-
-    int minCost(vector<int>& nums, int k) {
-        return solve(nums, 0, nums.size() - 1, k);   
-    }
+    return costs[0];
+  }
 };
