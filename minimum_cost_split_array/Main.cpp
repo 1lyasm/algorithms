3b0b335f53948baa87570cdd5cb4f9b724b794ce
1lyasm <il.mystafa@gmail.com>
2023-08-21 20:43:27 +0400

    Use std::array

--- minimum_cost_split_array/Main.cpp
+++ minimum_cost_split_array/Main.cpp
@@ -1,12 +1,13 @@
 class Solution {
  public:
   int minCost(vector<int>& nums, int k) {
-    int n = nums.size();
-    vector<int> costs(n + 1, INT_MAX / 2);
+    const int nMax = 1001, n = nums.size();
+    array<int, nMax> costs, counts;
+    costs.fill(INT_MAX / 2);
     costs[n] = 0;
     for (int i = n - 1; i >= 0; --i) {
       int len = 0;
-      vector<int> counts(n);
+      counts.fill(0);
       for (int j = i; j < n; ++j) {
         if (++counts[nums[j]] == 2)
           len += 2;
