e4a693690bf66c6c1c60b270b1bb4d1f9c236363
1lyasm <il.mystafa@gmail.com>
2023-10-01 19:03:08 +0300

    add init_candidates

--- apriori/src/main.rs
+++ apriori/src/main.rs
@@ -1,13 +1,47 @@
 struct Itemset {
-    item_list: Vec<String>
+    item_list: Vec<String>,
+    support: u64
 }
 
 struct Itemsets {
-    itemset_list: Vec<ItemSet>
+    itemset_list: Vec<Itemset>
 }
 
 struct Transactions {
     txn_list: Vec<Vec<&'static str>>,
+}
+
+struct Rule {
+    left: Vec<String>,
+    right: Vec<String>,
+}
+
+struct Rules {
+    rule_list: Vec<Rule>
+}
+
+impl Itemset {
+}
+
+impl Itemsets {
+    fn find_item_first(&self, target_item: String) -> (bool, usize) {
+        let (mut found, mut found_itemset) = (false, std::usize::MAX);
+        let mut i = 0;
+        while i < self.itemset_list.len() && !found {
+            let itemset = self.itemset_list.get(i).unwrap();
+            let mut j = 0;
+            while j < itemset.item_list.len() && !found {
+                let item = itemset.item_list.get(j).unwrap();
+                if item == &target_item {
+                    found_itemset = i;
+                    found = true;
+                }
+                j += 1;
+            }
+            i += 1;
+        }
+        return (found, found_itemset);
+    }
 }
 
 impl Transactions {
@@ -20,20 +54,26 @@ impl Transactions {
         }
     }
 
-
-    fn init_pass(&self) -> ItemSets {
-        return ItemSets{item_set_list: };
+    fn init_candidates(&self) -> Itemsets {
+        let mut candidates = Itemsets{itemset_list: Vec::new()};
+        for txn in &self.txn_list {
+            for item in txn {
+                let (contains, index) = candidates.find_item_first(item.to_string());
+                if contains {
+                    candidates.itemset_list.get_mut(index).unwrap().support += 1;
+                } else {
+                    candidates.itemset_list.push(
+                        Itemset{item_list: vec![item.to_string()], support: 1});
+                }
+            }
+        }
+        return candidates;
     }
 
     fn apriori(&self) -> Rules {
-        let candidates = self.init_pass();
+        let candidates = self.init_candidates();
         return Rules{rule_list: Vec::new()};
     }
-}
-
-struct Rule {
-    left: Vec<String>,
-    right: Vec<String>,
 }
 
 impl Rule {
@@ -47,10 +87,6 @@ impl Rule {
     }
 }
 
-struct Rules {
-    rule_list: Vec<Rule>
-}
-
 impl Rules {
     fn print(&self) {
         for rule in &self.rule_list {
@@ -61,6 +97,7 @@ impl Rules {
         }
     }
 }
+
 fn main() {
     let txn_list = Transactions{txn_list: vec![vec!["a", "b"], vec!["c", "d"]]};
     txn_list.print();
