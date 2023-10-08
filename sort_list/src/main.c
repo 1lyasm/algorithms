3729896d1d89448cd62d2f0c5c17ce51f6561e5e
1lyasm <il.mystafa@gmail.com>
2023-10-08 14:09:18 +0300

    change sort_list

--- sort_list/src/main.c
+++ sort_list/src/main.c
@@ -17,6 +17,7 @@ static unsigned getLen(struct ListNode *head) {
 }
 
 static struct ListNode *get(struct ListNode *head, unsigned index) {
+    printf("%s: index = %d\n\n", __func__, index);
   struct ListNode *current = head;
   for (unsigned i = 0; i < index; ++i) {
     current = current->next;
@@ -24,107 +25,171 @@ static struct ListNode *get(struct ListNode *head, unsigned index) {
   return current;
 }
 
-static void merge(struct ListNode *left, struct ListNode *right) {
-    printf("left = %p, right = %p\n\n",
-            (void *) left,
-            (void *) right);
-
-    if (!left || !right) {
-        return;
+void printList(struct ListNode *head) {
+  struct ListNode *current = head;
+  while (current) {
+    printf("%d", current->val);
+    if (current->next) {
+      printf(", ");
     }
+    current = current->next;
+  }
+  printf("\n");
+}
 
-    if (left->next == right && left->val > right->val) {
-        printf("neighbor\n\n");
-        int left_val = left->val;
-        left->val = right->val;
-        right->val = left_val;
-        return;
-    }
-  struct ListNode *current_left = left, *current_right = right;
-  struct ListNode *prev_left = left;
-  while (current_left != right) {
-    if (current_left->val < current_right->val) {
-      prev_left = current_left;
-      printf("prev_left = %p\n\n", (void *) prev_left);
-      if (current_left->next == right) {
-        current_left->next = current_right;
-        current_left = right;
-      } else {
-        current_left = current_left->next;
-        printf("next\n\n");
+static struct ListNode *merge(struct ListNode *left,
+                  struct ListNode *right) {
+  struct ListNode *cur_left = left, *cur_right = right, *initial_left = 0,
+                  *initial_right = 0, *right_next = 0, *last_left = left;
+  struct ListNode *newHead = left;
+  if (left->next == right) {
+      printf("neighbor\n\n");
+      right->next = left;
+      left->next = 0;
+      newHead = right;
+      return newHead;
+  }
+  unsigned right_ends = 0;
+  /* printf("cur_left = %d, cur_left->next = %d, cur_right = %d\n\n", */
+         /* cur_left->val, cur_left->next->val, cur_right->val); */
+  while (cur_left != right && !right_ends) {
+    if (cur_left->val <= cur_right->val) {
+      /* printf("cur_left->val = %d, cur_right->val = %d\n\n", cur_left->val, */
+             /* cur_right->val); */
+      initial_left = cur_left;
+      while (cur_left != right && cur_left->val <= cur_right->val) {
+        last_left = cur_left;
+        cur_left = cur_left->next;
+        /* printf("cur_left->next = %d\n\n", cur_left->next->val); */
+        /* printf("cur_left = %d\n\n", cur_left->val); */
+        /* printf("hey\n\n"); */
+      }
+      if (cur_left != right) {
+        initial_right = cur_right;
+        while (cur_right->next && cur_right->next->val <= cur_left->val) {
+          cur_right = cur_right->next;
+        }
+        initial_left->next = initial_right;
+        right_next = cur_right->next;
+        cur_right->next = cur_left;
+        cur_right = right_next;
       }
     } else {
-      prev_left->next = current_right;
-      current_right->next = current_left;
-      prev_left = current_right;
-      current_right = current_right->next;
+      /* printf("hey\n\n"); */
+      initial_right = cur_right;
+      while (cur_right->next && cur_right->next->val <= cur_left->val) {
+        cur_right = cur_right->next;
+      }
+      if (!cur_right->next) {
+        right_ends = 1;
+      }
+      cur_right->next = cur_left;
+      newHead = initial_right;
+      /* printf("cur_right = %d\n\n", cur_right->val); */
     }
   }
-}
-
-void printList(struct ListNode *head) {
-    struct ListNode *current = head;
-    while (current) {
-        printf("%d", current->val);
-        if (current->next) {
-            printf(", ");
-        }
-        current = current->next;
+  if (left->val <= right->val) {
+    if (last_left->val <= cur_right->val) {
+      last_left->next = cur_right;
+    } else {
+      last_left->next = 0;
     }
-    printf("\n");
+  } else {
+    cur_left = left;
+    while (cur_left != right) {
+      last_left = cur_left;
+      cur_left = cur_left->next;
+    }
+    last_left->next = 0;
+  }
+  /* printf("exiting merge\n\n"); */
+  return newHead;
 }
-
 
 static struct ListNode *sortList(struct ListNode *head) {
   unsigned len = getLen(head);
+  struct ListNode *newHead = head, *mergeHead;
   for (unsigned width = 1; width < len; width *= 2) {
-    for (unsigned i = 0; i < len; i += 2 * width) {
-        printf("index 1 = %u, index 2 = %u\n\n", i, i + width);
-      merge(get(head, i), get(head, i + width));
+    for (unsigned i = 0; i + width < len; i += 2 * width) {
+      printf("index 1 = %u, index 2 = %u\n\n", i, i + width);
+
+     mergeHead = merge(get(head, i), get(head, i + width));
+      if (i == 0) {
+          newHead = mergeHead;
+      }
+
+        printf("len: %u\n\n", getLen(head));
       printList(head);
     }
   }
-  return head;
+  return newHead;
 }
 
 struct ListNode *buildList(int *array, unsigned arrayLen) {
-    struct ListNode *head = malloc(sizeof(struct ListNode));
-    struct ListNode *current;
+  struct ListNode *head = malloc(sizeof(struct ListNode));
+  struct ListNode *current;
+  if (!head) {
+    printf("%s: malloc failed\n", __func__);
+    exit(EXIT_FAILURE);
+  }
+  head->val = array[0];
+  current = head;
+  for (unsigned i = 1; i < arrayLen; ++i) {
+    current->next = malloc(sizeof(struct ListNode));
     if (!head) {
-        printf("%s: malloc failed\n", __func__);
-        exit(EXIT_FAILURE);
+      printf("%s: malloc failed\n", __func__);
+      exit(EXIT_FAILURE);
     }
-    head->val = array[0];
-    current = head;
-    for (unsigned i = 1; i < arrayLen; ++i) {
-        current->next = malloc(sizeof(struct ListNode));
-        if (!head) {
-            printf("%s: malloc failed\n", __func__);
-            exit(EXIT_FAILURE);
-        }
-        current->next->val = array[i];
-        current = current->next;
-    }
-    current->next = 0;
-    return head;
+    current->next->val = array[i];
+    current = current->next;
+  }
+  current->next = 0;
+  return head;
 }
 
 void freeList(struct ListNode *head) {
-    struct ListNode *current = head;
-    while (current) {
-        struct ListNode *next = current->next;
-        free(current);
-        current = next;
-    }
+  struct ListNode *current = head;
+  while (current) {
+    struct ListNode *next = current->next;
+    free(current);
+    current = next;
+  }
 }
 
-int main() {
+void testMerge() {
+  {
+    int array[] = {3, 10, 11, 12, 4, 5, 6, 19};
+    unsigned arrayLen = sizeof(array) / sizeof(arrayLen);
+    struct ListNode *head = buildList(array, arrayLen);
+    printList(head);
+    head = merge(get(head, 0), get(head, 4));
+    printList(head);
+    freeList(head);
+  }
+  {
+    int array[] = {7, 8, 1, 2};
+    unsigned arrayLen = sizeof(array) / sizeof(arrayLen);
+    struct ListNode *head = buildList(array, arrayLen);
+    printList(head);
+    head = merge(get(head, 0), get(head, 2));
+    printList(head);
+    freeList(head);
+  }
+}
+
+void testSortList() {
+  {
     int array[] = {4, 2, 1, 3};
     unsigned arrayLen = sizeof(array) / sizeof(arrayLen);
     struct ListNode *head = buildList(array, arrayLen);
     printList(head);
-    sortList(head);
+    head = sortList(head);
     printList(head);
     freeList(head);
+  }
 }
 
+int main() {
+  testSortList();
+  /* testMerge(); */
+}
