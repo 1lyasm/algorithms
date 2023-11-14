4451c6bf71bcc748ccea137793aa277468ca8717
1lyasm <il.mystafa@gmail.com>
2023-11-14 19:48:58 +0300

    complete break_str

--- break_str/main.cpp
+++ break_str/main.cpp
@@ -1,63 +1,27 @@
 class Solution {
 public:
-    void printStrings(string &s1, string &s2) {
-        std::cout << "s1: " << s1 << ", s2: " << s2 << "\n";
+  bool checkIfCanBreak(string s1, string s2) {
+    bool res = false;
+    int dir = -1;
+    std::sort(s1.begin(), s1.end());
+    std::sort(s2.begin(), s2.end());
+    if (s1[0] > s2[0]) {
+      dir = 0;
+    } else if (s1[0] < s2[0]) {
+      dir = 1;
     }
-
-    vector<int> makeVec(string &s) {
-        vector<int> d;
-        d.reserve(s.size());
-        for (int i = 0; i < s.size(); ++i) {
-            d.push_back(s[i]);
+    for (int i = 0; i < s1.size(); ++i) {
+      if (dir == -1) {
+        if (s1[i] > s2[i]) {
+          dir = 0;
+        } else if (s1[i] < s2[i]) {
+          dir = 1;
         }
-        return d;
+      } else if (dir && s1[i] > s2[i] || !dir && s1[i] < s2[i]) {
+        return false;
+      }
     }
-
-    multiset<int> makeSet(string &s) {
-        multiset<int> set_;
-        for (int i = 0; i < s.size(); ++i) {
-            set_.insert(s[i]);
-        }
-        return set_;
-    }
-
-    bool checkIfCanBreak(string s1, string s2) {
-        bool res = false;
-        int dir = -1;
-        auto s1_s = makeSet(s1);
-        auto s2_s = makeSet(s2);
-        auto m1 = std::min_element(s1_s.begin(), s1_s.end());
-        auto m2 = std::min_element(s2_s.begin(), s2_s.end());
-        auto m1Val = *m1;
-        auto m2Val = *m2;
-        int curSize = s1_s.size();
-        if (m1Val > m2Val) {
-            dir = 0;
-        } else if (m1Val < m2Val) {
-            dir = 1;
-        }
-        do {
-            m1 = std::min_element(s1_s.begin(), s1_s.end());
-            m2 = std::min_element(s2_s.begin(), s2_s.end());
-            m1Val = *m1;
-            m2Val = *m2;
-            if (dir == -1) {
-                if (m1Val > m2Val) {
-                    dir = 0;
-                } else if (m1Val < m2Val) {
-                    dir = 1;
-                }
-            } else if (dir && m1Val > m2Val ||
-                !dir && m1Val < m2Val) {
-                return false;
-            }
-            s1_s.erase(m1);
-            s2_s.erase(m2);
-            if (s1_s.size() == 0) {
-                return true;
-            }
-        } while(1);
-        return res;
-    }
+    return true;
+  }
 };
 
