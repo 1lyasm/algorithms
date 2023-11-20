71909ad4247a3f5be69153d484a5f6cd62146b2e
1lyasm <il.mystafa@gmail.com>
2023-11-20 20:39:05 +0300

    complete hash_intersect

--- hash_intersect/src/main.c
+++ hash_intersect/src/main.c
@@ -1,10 +1,19 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
 
+void printArr(int *arr, int len) {
+    int i;
+    for (i = 0; i < len; ++i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 int *makeHash(int *arr, int len, int m) {
-    int *hash = malloc(m * sizeof(int)); int i;
+    int *hash = malloc(m * sizeof(int));
+    int i;
     if (!hash) {
         printf("malloc failed");
         exit(EXIT_FAILURE);
@@ -13,18 +22,25 @@ int *makeHash(int *arr, int len, int m) {
         hash[i] = -1;
     }
     for (i = 0; i < len; ++i) {
-        int hash_idx; int j;
+        int hash_idx;
+        int j;
         hash_idx = arr[i] % m;
+        // printf("makeHash: hash: \n\t");
+        // printArr(hash, m);
         j = 0;
-        while (j < m && hash[hash_idx] == -1) {
+        while (j < m && hash[hash_idx] != -1) {
             hash_idx = (hash_idx + 1) % m;
             ++j;
         }
         if (j == m) {
+            // printf("makeHash: j (%d) == m (%d)\n", j, m);
+            free(hash);
             return NULL;
         }
         hash[hash_idx] = arr[i];
     }
+    // printf("makeHash: hash: \n\t");
+    // printArr(hash, m);
     return hash;
 }
 
@@ -32,9 +48,11 @@ int searchHash(int *hash, int m, int val) {
     int hash_idx = val % m;
     int i = hash_idx;
     int j = 0;
+    // printf("hash_idx: %d\n", hash_idx);
     while (j < m && hash[hash_idx] != val && hash[hash_idx] != -1) {
         ++j;
         hash_idx = (hash_idx + 1) % m;
+        // printf("hash_idx: %d\n", hash_idx);
     }
     if (j == m) {
         return 0;
@@ -46,6 +64,17 @@ int searchHash(int *hash, int m, int val) {
 }
 
 int main() {
-    printf("hi\n");
+    int m = 11;
+    int i;
+    int a1[] = {3, 6, 2, 4, 1, 5};
+    int a2[] = {7, 4, 3, 2, 8, 5};
+    int n = sizeof(a1) / sizeof(a1[0]);
+    int *hash = makeHash(a1, n, m);
+    for (i = 0; i < n; ++i) {
+        if (searchHash(hash, m, a2[i]) == 1) {
+            printf("%d ", a2[i]);
+        }
+    }
+    printf("\n");
+    free(hash);
 }
-
