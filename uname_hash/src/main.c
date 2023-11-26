0a7099436c78c44c13a0463eef19d62115f2a20c
1lyasm <il.mystafa@gmail.com>
2023-11-26 17:52:11 +0300

    change main

--- uname_hash/src/main.c
+++ uname_hash/src/main.c
@@ -1,18 +1,55 @@
+#include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define N_MODE 2
+#define MAX_UNAME_LEN 256
 
-typedef enum { Normal, Debug } Mode;
+typedef enum { Normal, Debug, Error } Mode;
 
-Mode parseMode(char *arg) {
-    // if (strcmp(argv[1], "NORMAL") != 0 && strcmp(argc[1]))
-    // TODO return something
+void printMode(Mode mode) {
+    char *asStr = "error";
+    if (mode == Normal) {
+        asStr = "normal";
+    } else if (mode == Debug) {
+        asStr = "debug";
+    }
+    printf("printMode: Mode: %s\n", asStr);
+}
+
+Mode strToMode(char *str) {
+    Mode mode;
+    if (strcmp(str, "NORMAL") == 0) {
+        mode = Normal;
+    } else if (strcmp(str, "DEBUG") == 0) {
+        mode = Debug;
+    } else {
+        mode = Error;
+    }
+    return mode;
+}
+
+Mode parseMode(char *modeArg) {
+    char *modes[N_MODE] = {"NORMAL", "DEBUG"};
+    int i;
+    int isValid = 0;
+    Mode mode;
+    for (i = 0; i < N_MODE && isValid == 0; ++i) {
+        if (strcmp(modes[i], modeArg) != 0) {
+            isValid = 1;
+            mode = strToMode(modeArg);
+        }
+    }
+    if (isValid == 0) {
+        printf("Parametre NORMAL ve ya DEBUG değil\n");
+        exit(EXIT_FAILURE);
+    }
+    return mode;
 }
 
 Mode parseArgs(int argc, char **argv) {
-    char *modes[N_MODE] = {"NORMAL", "DEBUG"};
     char *modeArg;
     if (argc != 2) {
         printf("Program için 1 parametre gerekli: NORMAL ve ya DEBUG\n");
@@ -22,8 +59,118 @@ Mode parseArgs(int argc, char **argv) {
     return parseMode(modeArg);
 }
 
+int readN() {
+    int n = 0;
+    printf("N değerini giriniz: ");
+    scanf(" %d", &n);
+    return n;
+}
+
+double readLF() {
+    double lf;
+    printf("Load factor değerini giriniz: ");
+    scanf(" %lf", &lf);
+    return lf;
+}
+
+int checkPrime(int num) {
+    int i;
+    int isPrime = 1;
+    // printf("checkPrime: called with num: %d\n", num);
+    if (num <= 3) {
+        // printf("checkPrime: num (%d) <= 3\n", num);
+        return 1;
+    }
+    for (i = 2; i * i <= num && isPrime == 1; ++i) {
+        // printf("checkPrime: i: %d\n", i);
+        if (num % i == 0) {
+            // printf("checkPrime: i (%d) divides num (%d)\n", i, num);
+            isPrime = 0;
+        }
+    }
+    // printf("checkPrime: isPrime: %d\n", isPrime);
+    return isPrime;
+}
+
+int compM(int n, double lf) {
+    int quotient = (int)ceil(n / lf);
+    printf("compM: quotient: %d\n", quotient);
+    int m = quotient - 1;
+    int isPrime = 0;
+    while (isPrime == 0) {
+        ++m;
+        isPrime = checkPrime(m);
+    }
+    return m;
+}
+
+void testCheckPrime() {
+    assert(checkPrime(2) == 1);
+    assert(checkPrime(3) == 1);
+    assert(checkPrime(4) == 0);
+    assert(checkPrime(5) == 1);
+    assert(checkPrime(6) == 0);
+    assert(checkPrime(7) == 1);
+    assert(checkPrime(25) == 0);
+    assert(checkPrime(26) == 0);
+    assert(checkPrime(97) == 1);
+    assert(checkPrime(189) == 0);
+}
+
+void execAdd() {
+    char *uname = malloc(MAX_UNAME_LEN * sizeof(char));
+    if (uname == NULL) {
+        printf("execAdd: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("execAdd: called\n");
+    printf("Yeni kullanıcı adını giriniz: ");
+    scanf(" %s", uname);
+    printf("execAdd: uname: %s\n", uname);
+    free(uname);
+}
+
+void execDelete() { printf("execDelete: called\n"); }
+
+void execSearch() { printf("execSearch: called\n"); }
+
 int main(int argc, char **argv) {
-    parseArgs(argc, argv);
-    // TODO print something
+    Mode mode = parseArgs(argc, argv);
+    int n;
+    double lf;
+    int m;
+    char resp;
+    char **hash;
+    testCheckPrime();
+    printMode(mode);
+    n = readN();
+    printf("main: n: %d\n", n);
+    lf = readLF();
+    printf("main: lf: %lf\n", lf);
+    m = compM(n, lf);
+    printf("main: m: %d\n", m);
+    hash = calloc(m, sizeof(char *));
+    if (hash == NULL) {
+        printf("main: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    do {
+        int opId;
+        printf("\n\t1. Ekleme\n\t2. Silme\n\t3. Arama\n\n");
+        printf("İşlem: ");
+        scanf(" %c", &resp);
+        printf("main: resp: %c\n", resp);
+        opId = resp - '0';
+        if (opId == 1) {
+            execAdd();
+        } else if (opId == 2) {
+            execDelete();
+        } else if (opId == 3) {
+            execSearch();
+        } else {
+            printf("Geçersiz operasyon, yeniden deneyin\n");
+        }
+    } while (resp != 'q');
+    free(hash);
     return 0;
 }
